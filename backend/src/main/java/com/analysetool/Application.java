161e04615afe4cc0d8028eb9b2fa4076ce258aae
Tim <Tim.Niestrath@studmail.w-hs.de>
2023-04-06 11:53:21 +0200

    fixed repos not found

--- backend/src/main/java/com/analysetool/Application.java
+++ backend/src/main/java/com/analysetool/Application.java
@@ -1,4 +1,4 @@
-package main.java.com.analysetool;
+package com.analysetool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

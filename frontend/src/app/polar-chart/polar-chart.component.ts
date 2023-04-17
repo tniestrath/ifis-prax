63c4f09294a0a787b71992954b759fa343035ac6
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2023-04-17 13:02:55 +0200

    testing stuff

--- frontend/src/app/polar-chart/polar-chart.component.ts
+++ frontend/src/app/polar-chart/polar-chart.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { Chart } from 'chart.js/auto';
 import _default from "chart.js/dist/plugins/plugin.tooltip";
+import {CompanyService} from "../services/company.service";
+import {Company} from "../company-details/Company";
 
 @Component({
   selector: 'dash-polar-chart',
@@ -13,9 +15,12 @@ export class PolarChartComponent implements OnInit{
   @Input() labels : string[] = [];
   @Input() data : number[] = [];
 
+  constructor(private service: CompanyService) {
+  }
+
   createChart(labels : string[], data : number[]){
     this.chart = new Chart("chart", {
-      type: 'polarArea', data: {
+      type: 'doughnut', data: {
         labels: labels,
         datasets: [{
           label: "",
@@ -32,7 +37,27 @@ export class PolarChartComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.createChart(["#Hardware", "#Software", "#Home", "#VoIP"], [3, 1, 2, 6]);
+    //this.createChart(["#Hardware", "#Software", "#Home", "#VoIP"], [3, 1, 2, 6]);
+    let companies :Company[] = [];
+    let keywords : string[] = [];
+    let counts = {};
+    this.service.getAllCompanies().then(res => companies = res).finally(
+      () => {
+        for (const company of companies) {
+          let keys = company.keywords.split(",");
+          if (keys[0] == ""){
+            keys[0] = "None";
+          }
+          keys.forEach(key => keywords.push(key))
+        }
+        const map = keywords.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
+        map.delete("None");
+        map.forEach( (value, key, map) => {if(value < 0){map.delete(key)}})
+        this.createChart(Array.from(map.keys()), Array.from(map.values()));
+      }
+    );
+
+
   }
 }
 
--- frontend/src/app/services/company.service.ts
+++ frontend/src/app/services/company.service.ts
@@ -21,4 +21,5 @@ export class CompanyService {
     return await fetch(this.getAllUrl).then(res => res.json());
   }
 
+
 }

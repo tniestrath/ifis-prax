406944324e047893e91fe01df9bb7f1b41fda55c
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2023-06-05 14:51:42 +0200

    resolving weird errors

--- frontend/src/app/grid/grid-card.directive.spec.ts
+++ frontend/src/app/grid/grid-card.directive.spec.ts
@@ -2,6 +2,7 @@ import { GridCardDirective } from './grid-card.directive';
 
 describe('GridCardDirective', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     const directive = new GridCardDirective();
     expect(directive).toBeTruthy();
   });

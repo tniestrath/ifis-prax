b154b6783b571bd9be7fec1c41cce3cb7c585e03
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2023-06-12 14:59:09 +0200

    new selector.component options, to toggle "zebra colored" rows

--- frontend/src/app/page/selector/selector.component.ts
+++ frontend/src/app/page/selector/selector.component.ts
@@ -26,6 +26,7 @@ export class SelectorComponent implements OnInit{
   @Input() columnsForm = "repeat(3, 1fr)";
   @Input() padding : string = "5px 5vw";
   @Input() dataLoaded = new Observable<SelectorItem[]>();
+  @Input() zebraColorMode: boolean = false;
 
   @Output() itemClick = new EventEmitter<DbObject>();
 
@@ -55,7 +56,7 @@ export class SelectorComponent implements OnInit{
       const componentRef = viewContainerRef.createComponent<SelectableComponent>(item.component);
       componentRef.instance.data = item.data;
       componentRef.setInput("clicked", this.itemClick);
-      if (index % 2 == 0){
+      if (index % 2 == 0 && this.zebraColorMode){
         componentRef.location.nativeElement.setAttribute("style", "background : #EFEFEF");
       }
       index++;

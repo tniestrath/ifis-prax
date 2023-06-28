d0bed2c5ab6e151732e18cd41e887dc72431ea8d
Tim <Tim.Niestrath@studmail.w-hs.de>
2023-06-28 13:00:08 +0200

    fullfilled usermeta modell

--- backend/src/main/java/com/analysetool/modells/WPUserMeta.java
+++ backend/src/main/java/com/analysetool/modells/WPUserMeta.java
@@ -2,6 +2,8 @@ package com.analysetool.modells;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 
 @Entity
 @Table(name = "wp_usermeta")
@@ -20,4 +22,67 @@ public class WPUserMeta {
 
     @Column(name = "meta_value")
     private String value;
+
+    public WPUserMeta(){}
+
+    public WPUserMeta(Long id, Long userId, String key, String value) {
+        this.id = id;
+        this.userId = userId;
+        this.key = key;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof WPUserMeta that)) return false;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getUserId(), that.getUserId()) && Objects.equals(getKey(), that.getKey()) && Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUserId(), getKey(), getValue());
+    }
+
+    @Override
+    public String toString() {
+        return "WPUserMeta{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", key='" + key + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
 }

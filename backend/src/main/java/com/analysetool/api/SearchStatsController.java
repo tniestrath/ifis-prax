b0e83486176399ee5946cc61242585d41c9a770a
Tim <Tim.Niestrath@studmail.w-hs.de>
2023-08-08 15:10:27 +0200

    get search stat added to api

--- backend/src/main/java/com/analysetool/api/SearchStatsController.java
+++ backend/src/main/java/com/analysetool/api/SearchStatsController.java
@@ -2,7 +2,10 @@ package com.analysetool.api;
 
 import com.analysetool.modells.SearchStats;
 import com.analysetool.repositories.SearchStatsRepository;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +27,26 @@ public class SearchStatsController {
         return searchStatsRepository.findAll();
     }
 
+    @GetMapping("/getSearchStats")
+    public String getSearchStats(@RequestParam int limit) throws JSONException {
+        JSONArray response = new JSONArray();
+        List<SearchStats> alleStats = searchStatsRepository.findAll();
+        for (int i = alleStats.size() - 1; i != alleStats.size() - limit; i--) {
+
+            JSONObject obj = new JSONObject();
+            obj.put("search_string", alleStats.get(i).getSearchString());
+            obj.put("search_succes", alleStats.get(i).getSearchSuccessFlag());
+            if (alleStats.get(i).getClickedPost() != null) {
+
+                obj.put("clicked_post", alleStats.get(i).getClickedPost());
+
+            }
+            obj.put("location", alleStats.get(i).getLocation());
+            response.put(obj);
+        }
+
+        return response.toString();
+    }
 
 }
 

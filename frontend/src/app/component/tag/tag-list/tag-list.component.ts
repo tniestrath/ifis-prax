56945f32fc51c560c91631134a932758e6a0adc6
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2023-08-16 10:17:41 +0200

    tag-list.component now showing views instead of always empty performance

--- frontend/src/app/component/tag/tag-list/tag-list.component.ts
+++ frontend/src/app/component/tag/tag-list/tag-list.component.ts
@@ -18,11 +18,11 @@ export class TagListComponent extends DashBaseComponent implements AfterViewInit
   sorting_input_r : any;
   sorting_input_p : any;
   ngOnInit(): void {
-    this.setToolTip("Auflistung aller #Tags, sortierbar nach Relevanz oder Performance")
+    this.setToolTip("Auflistung aller #Tags, sortierbar nach Relevanz oder Views")
 
-    this.db.getAllTagsWithRelevanceAndPerformance().then(res => {
+    this.db.getAllTagsWithRelevanceAndViews().then(res => {
       for (var tag of res) {
-        this.selectorItems.push(new SelectorItem(TagListItemComponent, new TagRanking(tag.id, tag.name, tag.relevance, tag.performance, tag.count)));
+        this.selectorItems.push(new SelectorItem(TagListItemComponent, new TagRanking(tag.id, tag.name, tag.relevance, tag.views, tag.count)));
         this.selectorItems.sort((a, b) => (a.data as TagRanking).compareByRelevance((b.data as TagRanking)));
       }
       this.selectorItemsLoaded.next(this.selectorItems)
@@ -44,7 +44,7 @@ export class TagListComponent extends DashBaseComponent implements AfterViewInit
       this.selectorItemsLoaded.next(this.selectorItems);
     });
     this.sorting_input_p.addEventListener("change", () => {
-      this.selectorItems.sort((a, b) => (a.data as TagRanking).compareByPerformance((b.data as TagRanking)));
+      this.selectorItems.sort((a, b) => (a.data as TagRanking).compareByViews((b.data as TagRanking)));
       this.selectorItemsLoaded.next(this.selectorItems);
     });
   }

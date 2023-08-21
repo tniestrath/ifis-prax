9f4d6c7e4103aaec26a9acfb411fd9d78c80018d
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2023-08-21 11:31:40 +0200

    cookie-plugin update

--- frontend/src/app/component/login/login.component.ts
+++ frontend/src/app/component/login/login.component.ts
@@ -26,7 +26,6 @@ export class LoginComponent extends DashBaseComponent implements OnInit{
           this.styleLogin(true);
           return;
         }
-
         this.cs.set(ans.substring(0, ans.indexOf("=")), ans.substring(ans.indexOf("=")+1, ans.indexOf(";")), {expires: 1});
         this.db.getUserByLogin(ans.substring(ans.indexOf("=") + 1, ans.indexOf("|"))).then(res => {
           SysVars.login.next(res);

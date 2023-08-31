9a3f8cd0e2b162b0e71692947d9c890b0533c2f7
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2023-08-31 10:45:17 +0200

    JSON or HSON?

--- backend/src/main/java/com/analysetool/api/searchStatsController.java
+++ backend/src/main/java/com/analysetool/api/searchStatsController.java
@@ -4,8 +4,8 @@ import com.analysetool.modells.SearchStats;
 import com.analysetool.repositories.SearchStatsRepository;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

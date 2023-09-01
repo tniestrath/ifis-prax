230ba992754482bce0b6786416ac584fadb44358
JRGreen801 <robatjag@gmx.de>
2023-09-01 12:19:25 +0200

    fk routing only works internally

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'bericht',
-    redirectTo: '/api/bericht/gestern',
-    pathMatch: 'full'
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

5676b766bfc8d891736cd05e7f67b72ac8b37f76
JRGreen801 <robatjag@gmx.de>
2023-09-01 11:41:36 +0200

    first attempt routing api request to subpage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bericht',
+    redirectTo: '/api/bericht/gestern',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

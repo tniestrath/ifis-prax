26f1e70a3d5535569ee828229c55bc5565526d3c
pondgangster <majorshroom11@gmail.com>
2023-09-04 12:06:39 +0200

    OOPS I DID IT AGAIN

--- backend/src/main/java/com/analysetool/api/PostController.java
+++ backend/src/main/java/com/analysetool/api/PostController.java
@@ -833,7 +833,7 @@ public class PostController {
 
         for(Post post : posts) {
             JSONObject json = PostStatsByIdForFrontend(post.getId());
-            if(!json.get("type").toString().contains("Ratgeber") && !json.get("tags").toString().contains("Whitepaper")) {
+            if(!json.get("type").toString().toLowerCase().contains("ratgeber") && !json.get("tags").toString().toLowerCase().contains("whitepaper")) {
                 stats.add(this.PostStatsByIdForFrontend(post.getId()));
             }
         }

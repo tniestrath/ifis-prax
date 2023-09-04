f390e2089b0540687f8cd014d10fb927554b4435
pondgangster <majorshroom11@gmail.com>
2023-09-04 12:12:39 +0200

    Changed to whitelist, too many post-types added (i mean, a podcast, really?)

--- backend/src/main/java/com/analysetool/api/PostController.java
+++ backend/src/main/java/com/analysetool/api/PostController.java
@@ -833,7 +833,9 @@ public class PostController {
 
         for(Post post : posts) {
             JSONObject json = PostStatsByIdForFrontend(post.getId());
-            if(!json.get("type").toString().toLowerCase().contains("ratgeber") && !json.get("type").toString().toLowerCase().contains("whitepaper")) {
+            if(json.get("type").toString().toLowerCase().contains("blog")  ||
+                    json.get("type").toString().toLowerCase().contains("news") ||
+                    json.get("type").toString().toLowerCase().contains("article")) {
                 stats.add(this.PostStatsByIdForFrontend(post.getId()));
             }
         }

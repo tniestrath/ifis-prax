bddab018c47c6a4bce4859d8ca432ba25efed21f
pondgangster <majorshroom11@gmail.com>
2023-10-09 13:11:28 +0200

    Added CallupByCategory and CallupByCategoryLive

--- backend/src/main/java/com/analysetool/repositories/UniversalCategoriesDLCRepository.java
+++ backend/src/main/java/com/analysetool/repositories/UniversalCategoriesDLCRepository.java
@@ -2,7 +2,10 @@ package com.analysetool.repositories;
 
 import com.analysetool.modells.UniversalCategoriesDLC;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UniversalCategoriesDLCRepository extends JpaRepository<UniversalCategoriesDLC, Integer> {
+
+     public UniversalCategoriesDLC getById(int id);
 
 }

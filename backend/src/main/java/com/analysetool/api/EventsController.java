f835283ef6cee183772338c827c7dc26675907c4
pondgangster <majorshroom11@gmail.com>
2023-10-31 09:54:17 +0100

    You can now get a list of upcoming/current Events with their names and status

--- backend/src/main/java/com/analysetool/api/EventsController.java
+++ backend/src/main/java/com/analysetool/api/EventsController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(originPatterns = "*" , allowCredentials = "true")
@@ -64,6 +67,41 @@ public class EventsController {
             }
         }
         return count;
+    }
+
+    /**
+     *
+     * @return a List of Strings, each starting with c| (current) or u| (upcoming) and then the name of the event.
+     */
+    @GetMapping("/getAmountOfEventsCreatedYesterday")
+    public List<String> getAmountOfEventsCreatedYesterday() {
+        List<String> events = new ArrayList<>();
+        List<Events> allEvents = eventsRepo.findAll();
+        LocalDate today = LocalDate.now();
+
+        for (Events e : allEvents) {
+            LocalDate createdDate = e.getEventDateCreated().toLocalDate();
+
+            if (createdDate.isBefore(today)) {
+                if(isCurrent(e)) {
+                    events.add("c|" + e.getEventName());
+                } else if(isUpcoming(e)) {
+                    events.add("u|" + e.getEventName());
+                }
+
+            }
+        }
+        return events;
+    }
+
+    private boolean isCurrent(Events e) {
+        LocalDateTime now = LocalDateTime.now();
+        return e.getEventStart().isBefore(now) && e.getEventEnd().isAfter(now);
+    }
+
+    private boolean isUpcoming(Events e) {
+        LocalDateTime now = LocalDateTime.now();
+        return e.getEventStart().isAfter(now);
     }
 
 }

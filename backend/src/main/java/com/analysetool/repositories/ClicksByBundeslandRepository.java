be0e841b769beec71457f51a69423de650584318
pondgangster <majorshroom11@gmail.com>
2023-11-07 13:35:47 +0100

    i I deine Mutter wirklich

--- backend/src/main/java/com/analysetool/repositories/ClicksByBundeslandRepository.java
+++ backend/src/main/java/com/analysetool/repositories/ClicksByBundeslandRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClicksByBundeslandRepository extends JpaRepository<ClicksByBundesland, Long> {
 
-    @Query("SELECT u FROM ClicksByBundesland u WHERE u.uniStatId =:uniStatid AND u.bundesland=:bundesland")
+    @Query("SELECT u FROM ClicksByBundesland u WHERE u.uniStatId =:uniStatId AND u.bundesland=:bundesland")
     public ClicksByBundesland getByUniIDAndBundesland(int uniStatId, String bundesland);
 }

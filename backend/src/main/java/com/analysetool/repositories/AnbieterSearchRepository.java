4c831fa8ef980f86cc8457698ecac64d99983f6f
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2023-11-09 16:17:58 +0100

    added outliers for provider search in search stat api

--- backend/src/main/java/com/analysetool/repositories/AnbieterSearchRepository.java
+++ backend/src/main/java/com/analysetool/repositories/AnbieterSearchRepository.java
@@ -2,9 +2,15 @@ package com.analysetool.repositories;
 
 import com.analysetool.modells.AnbieterSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AnbieterSearchRepository extends JpaRepository<AnbieterSearch, Long> {
 
+/*    @Query("SELECT a from AnbieterSearch a order by a.id desc LIMIT =:x")
+    List<AnbieterSearch> findLastX(int x);*/
+    List<AnbieterSearch>findTop15ById();
 }

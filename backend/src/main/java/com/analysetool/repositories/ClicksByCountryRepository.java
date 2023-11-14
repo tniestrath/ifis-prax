ef03c852e61294c456ae7b83ca3f109061a70d30
pondgangster <majorshroom11@gmail.com>
2023-11-14 11:50:41 +0100

    Added totalGermanGeo in GeoController - it's supposed to give you an ordered List of clicks per Bundesland in a range of dates

--- backend/src/main/java/com/analysetool/repositories/ClicksByCountryRepository.java
+++ backend/src/main/java/com/analysetool/repositories/ClicksByCountryRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClicksByCountryRepository extends JpaRepository<ClicksByCountry, Long> {
 
     @Query("SELECT u FROM ClicksByCountry u WHERE u.uniStatId =:uniStatId AND u.country=:country")
     public ClicksByCountry getByUniIDAndCountry(int uniStatId, String country);
+
+    @Query("SELECT u FROM ClicksByCountry u WHERE u.uniStatId =:uniStatId")
+    List<ClicksByCountry> getByUniID(int uniStatId);
 }

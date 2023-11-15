88b32be356cf194b8d0f256afc3adc5218e84593
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2023-11-15 19:19:58 +0100

    event search added (snipped made me go aua aua))

---
+++ backend/src/main/java/com/analysetool/repositories/EventSearchRepository.java
@@ -0,0 +1,8 @@
+package com.analysetool.repositories;
+
+import com.analysetool.modells.EventSearch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventSearchRepository extends JpaRepository<EventSearch, Long> {
+
+}

46a6b8dafcd57c0ac4684d2cb87e5779aad659f2
pondgangster <majorshroom11@gmail.com>
2023-11-29 10:11:40 +0100

    added documentation and deleted unnecessary modells and Repository (and their respective table since it uses deprecated columns)

--- backend/src/main/java/com/analysetool/repositories/PodCastStatsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.analysetool.repositories;
-import com.analysetool.modells.PodCastStats;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PodCastStatsRepository extends JpaRepository<PodCastStats, Long> {
-    // Define custom query methods here
-}

c3b6601c458bbc2c6350ca582735397712276467
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2023-12-05 15:30:22 +0100

    added average time spend & average time spend today to controller

--- backend/src/main/java/com/analysetool/api/UniqueUserController.java
+++ backend/src/main/java/com/analysetool/api/UniqueUserController.java
@@ -3,8 +3,11 @@ package com.analysetool.api;
 import com.analysetool.repositories.UniqueUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @CrossOrigin(originPatterns = "*" , allowCredentials = "true")
 @RestController
@@ -13,5 +16,21 @@ public class UniqueUserController {
 
     @Autowired
     UniqueUserRepository uniqueUserRepo;
+
+    // Endpoint, um die durchschnittliche Verweildauer aller Nutzer als String zurückzugeben
+    @GetMapping("/average-time-spent")
+    public String getAverageTimeSpent() {
+        Double averageTimeSpent = uniqueUserRepo.getAverageTimeSpent();
+        return averageTimeSpent != null ? String.format("%.2f", averageTimeSpent) : "Daten nicht verfügbar";
+    }
+
+    // Endpoint, um die durchschnittliche Verweildauer der Nutzer für den heutigen Tag als String zurückzugeben
+    @GetMapping("/average-time-spent-today")
+    public String getTodayAverageTimeSpent() {
+        LocalDateTime startOfDay = LocalDateTime.now().toLocalDate().atStartOfDay();
+        LocalDateTime endOfDay = LocalDateTime.now().toLocalDate().atTime(23, 59, 59);
+        Double todayAverageTimeSpent = uniqueUserRepo.getTodayAverageTimeSpent(startOfDay, endOfDay);
+        return todayAverageTimeSpent != null ? String.format("%.2f", todayAverageTimeSpent) : "Daten nicht verfügbar";
+    }
 
 }
--- backend/src/main/java/com/analysetool/repositories/UniqueUserRepository.java
+++ backend/src/main/java/com/analysetool/repositories/UniqueUserRepository.java
@@ -3,7 +3,10 @@ package com.analysetool.repositories;
 import com.analysetool.modells.UniqueUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface UniqueUserRepository extends JpaRepository<UniqueUser, Long> {
@@ -13,4 +16,12 @@ public interface UniqueUserRepository extends JpaRepository<UniqueUser, Long> {
 
     @Query("SELECT u FROM UniqueUser u WHERE u.ip = :ip")
     public UniqueUser findByIP(String ip);
+
+    // Methode, um die durchschnittliche Verweildauer aller Nutzer zurückzugeben
+    @Query("SELECT AVG(u.time_spent) FROM UniqueUser u")
+    Double getAverageTimeSpent();
+
+    // Methode, um die durchschnittliche Verweildauer der Nutzer für einen Tag zurückzugeben
+    @Query("SELECT AVG(u.time_spent) FROM UniqueUser u WHERE u.first_click >= :startOfDay AND u.first_click <= :endOfDay")
+    Double getTodayAverageTimeSpent(@Param("startOfDay") LocalDateTime startOfDay, @Param("endOfDay") LocalDateTime endOfDay);
 }

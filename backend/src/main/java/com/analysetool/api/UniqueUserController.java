ae1a6582e0a9a8e38ebde15cfe3f31e9f52a3fce
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2023-12-06 18:35:12 +0100

    added getuserpaths to unique user controller (i hope it works)

--- backend/src/main/java/com/analysetool/api/UniqueUserController.java
+++ backend/src/main/java/com/analysetool/api/UniqueUserController.java
@@ -1,10 +1,20 @@
 package com.analysetool.api;
 
+import com.analysetool.modells.UniqueUser;
 import com.analysetool.repositories.UniqueUserRepository;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 @CrossOrigin(originPatterns = "*" , allowCredentials = "true")
 @RestController
@@ -30,17 +40,9 @@ public class UniqueUserController {
         return todayAverageTimeSpent != null ? String.format("%.2f", todayAverageTimeSpent) : "Daten nicht verfügbar";
     }
 
-/*
-    */
-/**
-     * Berechnet die durchschnittliche Verweildauer der Nutzer für einen spezifizierten Zeitraum.
-     * Der Zeitraum wird durch 'daysBackFrom' und 'daysBackTo' relativ zum aktuellen Datum definiert.
-     *
-     * @param daysBackFrom Start des Zeitraums in Tagen vor heute (z.B. 7 für 7 Tage vor heute).
-     * @param daysBackTo   Ende des Zeitraums in Tagen vor heute (z.B. 3 für 3 Tage vor heute).
-     * @return Durchschnittliche Verweildauer als String oder 'Daten nicht verfügbar' bei fehlenden Daten.
-     *//*
 
+
+/*
     @GetMapping("/average-time-spent-range")
     public String getAverageTimeSpentInRange(@RequestParam("daysBackFrom") int daysBackFrom, @RequestParam("daysBackTo") int daysBackTo) {
         LocalDateTime startDate = LocalDateTime.now().minusDays(daysBackFrom).toLocalDate().atStartOfDay();
@@ -50,5 +52,63 @@ public class UniqueUserController {
         return averageTimeSpent != null ? String.format("%.2f", averageTimeSpent) : "Daten nicht verfügbar";
     }
 */
+
+    /**
+     * Retrieves the click paths of users who have more than two clicks.
+     * This method returns the click paths as a single string, with each path separated by commas.
+     * Each path represents the sequence of categories clicked by a user.
+     * The number of users' paths returned is limited by the provided 'limit' parameter.
+     *
+     * @param limit The maximum number of user paths to return. If not specified, defaults to 10.
+     *              This limits the number of users to be considered for generating click paths.
+     * @return A String containing the click paths of users, separated by commas.
+     *         Each click path is a sequence of category names representing the order of clicks.
+     *         Returns an empty string if no valid paths are found or if an error occurs.
+     */
+    @GetMapping("/paths")
+    public String getUserPaths(@RequestParam(defaultValue = "10") int limit) {
+        Pageable topLimit = PageRequest.of(0, limit, Sort.by(Sort.Direction.DESC, "id"));
+        List<UniqueUser> users = uniqueUserRepo.findTopByMoreThanTwoClicks(topLimit);
+
+        return users.stream()
+                .map(this::reconstructClickPath)
+                .collect(Collectors.joining(", "));
+    }
+
+    private String reconstructClickPath(UniqueUser user) {
+        Map<Integer, String> clickMap = new TreeMap<>();
+        try{
+        processCategoryClicks(user.getArticle(), "article", clickMap);
+        processCategoryClicks(user.getBlog(), "blog", clickMap);
+        processCategoryClicks(user.getNews(), "news", clickMap);
+        processCategoryClicks(user.getWhitepaper(), "whitepaper", clickMap);
+        processCategoryClicks(user.getPodcast(), "podcast", clickMap);
+        processCategoryClicks(user.getRatgeber(), "ratgeber", clickMap);
+        processCategoryClicks(user.getMain(), "main", clickMap);
+        processCategoryClicks(user.getUeber(), "ueber", clickMap);
+        processCategoryClicks(user.getImpressum(), "impressum", clickMap);
+        processCategoryClicks(user.getPreisliste(), "preisliste", clickMap);
+        processCategoryClicks(user.getPartner(), "partner", clickMap);
+        processCategoryClicks(user.getDatenschutz(), "datenschutz", clickMap);
+        processCategoryClicks(user.getNewsletter(), "newsletter", clickMap);
+        processCategoryClicks(user.getImage(), "image", clickMap);
+        processCategoryClicks(user.getAgb(), "agb", clickMap);}
+        catch (Exception e){System.out.println("computer sagt nein");}
+
+        return clickMap.values().stream()
+                .collect(Collectors.joining(","));
+    }
+
+    private void processCategoryClicks(String categoryData, String categoryName, Map<Integer, String> clickMap) throws JSONException {
+        if (categoryData != null && !categoryData.isEmpty()) {
+            JSONArray clicksArray = new JSONArray(categoryData);
+            for (int i = 0; i < clicksArray.length(); i++) {
+                int clickNum = clicksArray.getInt(i);
+                if (clickNum > 0) {
+                    clickMap.put(clickNum, categoryName);
+                }
+            }
+        }
+    }
 
 }
--- backend/src/main/java/com/analysetool/repositories/UniqueUserRepository.java
+++ backend/src/main/java/com/analysetool/repositories/UniqueUserRepository.java
@@ -1,12 +1,14 @@
 package com.analysetool.repositories;
 
 import com.analysetool.modells.UniqueUser;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface UniqueUserRepository extends JpaRepository<UniqueUser, Long> {
@@ -24,4 +26,7 @@ public interface UniqueUserRepository extends JpaRepository<UniqueUser, Long> {
     // Methode, um die durchschnittliche Verweildauer der Nutzer für einen Tag zurückzugeben
     @Query("SELECT AVG(u.time_spent) FROM UniqueUser u WHERE u.first_click >= :startOfDay AND u.first_click <= :endOfDay")
     Double getAverageTimeSpentBetweenDates(@Param("startOfDay") LocalDateTime startOfDay, @Param("endOfDay") LocalDateTime endOfDay);
+
+    @Query("SELECT u FROM UniqueUser u WHERE u.amount_of_clicks > 2")
+    List<UniqueUser> findTopByMoreThanTwoClicks(Pageable pageable);
 }

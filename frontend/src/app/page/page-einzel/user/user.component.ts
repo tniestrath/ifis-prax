a6648240d5fcccc80e182edfe43d7cc34bbd87f4
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2023-12-20 12:28:39 +0100

    user.component tendency

--- frontend/src/app/page/page-einzel/user/user.component.ts
+++ frontend/src/app/page/page-einzel/user/user.component.ts
@@ -16,7 +16,8 @@ export class UserComponent implements SelectableComponent, OnInit {
 
   user_img: SafeUrl = "";
   bgColor: string = "0";
-  performance_image_id: string = "33";
+
+  tendency: string = "stagniert";
 
   constructor(private db: DbService) {
   }
@@ -26,9 +27,9 @@ export class UserComponent implements SelectableComponent, OnInit {
   }
 
   ngOnInit(): void {
-    if (this.data.performance <= 33) this.performance_image_id = "33";
-    else if (this.data.performance > 33 && this.data.performance <= 66) this.performance_image_id = "66";
-    else if (this.data.performance > 66) this.performance_image_id = "100";
+    if (this.data.tendency == null) this.tendency = "stagniert"
+    else if (!this.data.tendency) this.tendency = "fallend"
+    else if (this.data.tendency) this.tendency = "steigend";
 
     this.db.getUserImgSrc(this.data.id).then(dataUrl => {
       this.user_img = dataUrl;

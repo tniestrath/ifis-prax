72fe9951f81c2896c29d2630938574750ab9ecf6
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2023-12-21 20:21:35 +0100

    more methods for contentDownloadsHourlyService

--- backend/src/main/java/com/analysetool/repositories/ContentDownloadsHourlyRepository.java
+++ backend/src/main/java/com/analysetool/repositories/ContentDownloadsHourlyRepository.java
@@ -2,6 +2,7 @@ package com.analysetool.repositories;
 
 import com.analysetool.modells.ContentDownloadsHourly;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,14 @@ import java.util.List;
 public interface ContentDownloadsHourlyRepository extends JpaRepository<ContentDownloadsHourly, Integer> {
     List<ContentDownloadsHourly> findAllByPostId(Long postId);
     List<ContentDownloadsHourly> findAllByPostIdAndUniId(Long postId,Integer uniId);
+    boolean existsByPostId(long postId);
+
+    @Query("SELECT c.uniId FROM ContentDownloadsHourly c WHERE c.postId=:postId ORDER BY c.uniId ASC LIMIT 1")
+    long getFirstUniIdByPostId(long postId);
+
+    @Query("SELECT c.uniId FROM ContentDownloadsHourly c ORDER BY c.uniId DESC LIMIT 1")
+    long getLastUniId();
+    @Query("SELECT c.uniId FROM ContentDownloadsHourly c ORDER BY c.uniId DESC LIMIT 7")
+    List<Integer> getLast7Uni();
+
 }
--- backend/src/main/java/com/analysetool/services/ContentDownloadsHourlyService.java
+++ backend/src/main/java/com/analysetool/services/ContentDownloadsHourlyService.java
@@ -1,5 +1,6 @@
 package com.analysetool.services;
 import com.analysetool.modells.ContentDownloadsHourly;
+import com.analysetool.modells.UserViewsByHourDLC;
 import com.analysetool.repositories.ContentDownloadsHourlyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,13 @@ public class ContentDownloadsHourlyService {
             contentDownloadsHourlyRepo.saveAll(contentDownloadsMap.values());
         }
     }
+    private int getDaysSinceTracking(long postId) {
+        if(contentDownloadsHourlyRepo.existsByPostId(postId)) {
+            return (int) (contentDownloadsHourlyRepo.getLastUniId() - contentDownloadsHourlyRepo.getFirstUniIdByPostId(postId));
+        } else {
+            return 0;
+        }
+    }
 
     public Long getAllContentDownloadsByPostId(Long postId){
         List<ContentDownloadsHourly> downloadsList = contentDownloadsHourlyRepo.findAllByPostId(postId);
@@ -35,6 +43,40 @@ public class ContentDownloadsHourlyService {
                 .mapToLong(ContentDownloadsHourly::getDownloads)
                 .sum();
         return totalDownloads;
+    }
+
+    public double getDownloadsPerDay(long postId) {
+        int countDays = getDaysSinceTracking(postId);
+        long totalDownloads = 0;
+        int lastUniId = 0;
+        for(ContentDownloadsHourly c : contentDownloadsHourlyRepo.findAllByPostId(postId)) {
+            if(lastUniId != c.getUniId()) {
+                lastUniId = c.getUniId();
+            }
+            totalDownloads= c.getDownloads();
+        }
+        if(countDays > 0) {
+            return (double) totalDownloads / countDays;
+        } else {
+            return 0;
+        }
+    }
+    public Boolean tendencyUp(long postId) {
+        int count = 7;
+        int downloads = 0;
+        if(getDaysSinceTracking(postId) > 7) {
+            for(Integer uni : contentDownloadsHourlyRepo.getLast7Uni()) {
+                for(ContentDownloadsHourly c : contentDownloadsHourlyRepo.findAllByPostIdAndUniId(postId, uni)) {
+                    downloads += c.getDownloads();
+                }
+            }
+        } else {
+            return null;
+        }
+        Double avg = ((double) downloads / count);
+        if(avg > getDownloadsPerDay(postId)) return true;
+        if(avg.equals(getDownloadsPerDay(postId))) return null;
+        return false;
     }
 
 

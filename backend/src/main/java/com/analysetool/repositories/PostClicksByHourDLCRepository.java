d3fd73b167e71d090c65fa54b052ea814bb8916b
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2023-12-28 17:47:08 +0100

    added getPostClicksOfLast24HourByPostIdAndDaysBackDistributedByHour to Service

--- backend/src/main/java/com/analysetool/repositories/PostClicksByHourDLCRepository.java
+++ backend/src/main/java/com/analysetool/repositories/PostClicksByHourDLCRepository.java
@@ -4,8 +4,11 @@ import com.analysetool.modells.PostClicksByHourDLC;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostClicksByHourDLCRepository extends JpaRepository<PostClicksByHourDLC, Integer> {
 
+    List<PostClicksByHourDLC> findAllByPostIdAndUniId(Long postId, Integer uniId);
 }
 

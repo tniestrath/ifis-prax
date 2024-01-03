24683f8be8e0f3d924e6b22d109f15af93bbd408
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2024-01-03 16:37:28 +0100

    methods for service

--- backend/src/main/java/com/analysetool/services/UserRedirectsHourlyService.java
+++ backend/src/main/java/com/analysetool/services/UserRedirectsHourlyService.java
@@ -25,7 +25,43 @@ public class UserRedirectsHourlyService {
         }
     }
 
+    private int getDaysSinceTracking(long userId) {
+        if (userRedirectRepo.existsByUserId(userId)) {
+            return (int) (userRedirectRepo.getLastUniId() - userRedirectRepo.getFirstUniIdByUserId(userId));
+        } else {
+            return 0;
+        }
+    }
 
+    public Long getTotalRedirectsOfSite() {
+        return userRedirectRepo.getAllRedirectsSummed();
+    }
+
+    public Long getAllRedirectsByUserId(Long userId) {
+        return userRedirectRepo.getAllRedirectsOfUserIdSummed(userId);
+    }
+
+    public double getRedirectsPerDay(long userId) {
+        int countDays = getDaysSinceTracking(userId);
+        long totalRedirects = userRedirectRepo.getAllRedirectsOfUserIdSummed(userId);
+        return countDays > 0 ? (double) totalRedirects / countDays : 0;
+    }
+
+    public Boolean tendencyUp(long userId) {
+        int count = 7;
+        long redirects = 0;
+        if(getDaysSinceTracking(userId) > 7) {
+            for(Integer uni : userRedirectRepo.getLast7Uni()) {
+                redirects += userRedirectRepo.findAllByUserIdAndUniId(userId, uni).stream()
+                        .mapToLong(UserRedirectsHourly::getRedirects)
+                        .sum();
+            }
+        } else {
+            return null;
+        }
+        Double avg = ((double) redirects / count);
+        return avg > getRedirectsPerDay(userId);
+    }
 
 
 

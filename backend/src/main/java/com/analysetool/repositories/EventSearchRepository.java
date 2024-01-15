7f41e81ab60b6de24181ddcb9712ec1a9536f70b
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2024-01-15 17:17:35 +0100

    getZeroCountEventSearches added to SearchStat controller

--- backend/src/main/java/com/analysetool/repositories/EventSearchRepository.java
+++ backend/src/main/java/com/analysetool/repositories/EventSearchRepository.java
@@ -4,9 +4,14 @@ import com.analysetool.modells.EventSearch;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EventSearchRepository extends JpaRepository<EventSearch, Long> {
 
     Page<EventSearch> findAllByOrderByIdDesc(Pageable pageable);
 
+    @Query("SELECT es FROM EventSearch es WHERE es.resultCount=0")
+    List<EventSearch> getEventSearchesWithCountZero();
 }

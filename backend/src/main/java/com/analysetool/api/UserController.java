45c89d1ba896266cc60492c173ef6a84e6e18efb
pondgangster <majorshroom11@gmail.com>
2024-01-16 10:43:31 +0100

    userId renamed

--- backend/src/main/java/com/analysetool/api/UserController.java
+++ backend/src/main/java/com/analysetool/api/UserController.java
@@ -499,12 +499,12 @@ public class UserController {
      * and a count of all events by this user that are active (countTotal).
      */
     @GetMapping("/getAmountOfEvents")
-    public String getCountEvents(long userId) throws JSONException {
+    public String getCountEvents(long id) throws JSONException {
 
         JSONObject json = new JSONObject();
 
         List<String> events = new ArrayList<>();
-        List<Events> allEvents = eventsRepo.getAllByOwnerID(userId);
+        List<Events> allEvents = eventsRepo.getAllByOwnerID(id);
         int countOld = 0;
 
         for (Events e : allEvents) {
@@ -571,9 +571,9 @@ public class UserController {
      * @return a List of Strings, each starting with c| (current) or u| (upcoming) and then the name of the event for all events created within the last day, by the given User.
      */
     @GetMapping("/getAmountOfEventsCreatedYesterday")
-    public List<String> getAmountOfEventsCreatedYesterday(long userId) {
+    public List<String> getAmountOfEventsCreatedYesterday(long id) {
         List<String> events = new ArrayList<>();
-        List<Events> allEvents = eventsRepo.getAllByOwnerID(userId);
+        List<Events> allEvents = eventsRepo.getAllByOwnerID(id);
         LocalDate today = LocalDate.now();
 
         for (Events e : allEvents) {

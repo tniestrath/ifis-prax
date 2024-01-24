9613400c329d1482d95e65a403a27e3c6daaaaf5
pondgangster <majorshroom11@gmail.com>
2024-01-24 12:22:39 +0100

    changed ordering for postsByAuthor

--- backend/src/main/java/com/analysetool/repositories/PostRepository.java
+++ backend/src/main/java/com/analysetool/repositories/PostRepository.java
@@ -31,10 +31,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
    List<Post> findByAuthorIdAndStatusAndType(long userId, String status, String type, Pageable pageable);
 
-   @Query("SELECT p FROM Post p LEFT JOIN wp_term_relationships wtr ON wtr.objectId = p.id LEFT JOIN WpTermTaxonomy wtt ON wtr.termTaxonomyId = wtt.termTaxonomyId LEFT JOIN WPTerm wpt ON wpt.id = wtt.termId WHERE p.status= 'publish' AND p.type='post' AND p.authorId=:userId AND (p.title LIKE %:search% OR p.content LIKE %:search%) AND wpt.slug =:filter")
+   @Query("SELECT p FROM Post p LEFT JOIN wp_term_relationships wtr ON wtr.objectId = p.id LEFT JOIN WpTermTaxonomy wtt ON wtr.termTaxonomyId = wtt.termTaxonomyId LEFT JOIN WPTerm wpt ON wpt.id = wtt.termId WHERE p.status= 'publish' AND p.type='post' AND p.authorId=:userId AND (p.title LIKE %:search% OR p.content LIKE %:search%) AND wpt.slug =:filter ORDER BY p.date DESC")
    List<Post> findByAuthorPageable(long userId, String search, String filter, Pageable pageable);
 
-   @Query("SELECT p FROM Post p WHERE p.status= 'publish' AND p.type='post' AND p.authorId=:userId AND (p.title LIKE %:search% OR p.content LIKE %:search%)")
+   @Query("SELECT p FROM Post p WHERE p.status= 'publish' AND p.type='post' AND p.authorId=:userId AND (p.title LIKE %:search% OR p.content LIKE %:search%) ORDER BY p.date DESC")
    List<Post> findByAuthorPageable(long userId, String search, Pageable pageable);
 
    @Query("SELECT p.id from Post p where p.title =:title")

098c06dbefc862a43764c9e74b6f041161f35150
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2024-02-06 09:42:59 +0100

    ups

--- backend/src/main/java/com/analysetool/api/LoginController.java
+++ backend/src/main/java/com/analysetool/api/LoginController.java
@@ -19,10 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;

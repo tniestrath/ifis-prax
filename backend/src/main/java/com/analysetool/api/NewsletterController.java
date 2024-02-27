40fb8dddf9a6441066ea27870090439ea8dadfc5
pondgangster <majorshroom11@gmail.com>
2024-02-27 12:41:31 +0100

    Geo implemented, callups implemented, list of callups

--- backend/src/main/java/com/analysetool/api/NewsletterController.java
+++ backend/src/main/java/com/analysetool/api/NewsletterController.java
@@ -1,14 +1,18 @@
 package com.analysetool.api;
 
 import com.analysetool.modells.Newsletter;
+import com.analysetool.modells.NewsletterEmails;
 import com.analysetool.modells.NewsletterStats;
 import com.analysetool.repositories.NewsletterEmailsRepository;
 import com.analysetool.repositories.NewsletterRepository;
 import com.analysetool.repositories.NewsletterSentRepository;
 import com.analysetool.repositories.NewsletterStatsRepository;
+import com.analysetool.util.IPHelper;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -151,5 +155,92 @@ public class NewsletterController {
         }
 
     }
+
+    @GetMapping("/getNewsletterGeoSingle")
+    public String getNewsletterGeo(int emailId) throws JSONException {
+       JSONObject json = new JSONObject();
+
+       for(NewsletterStats n : newsStatsRepo.getAllNewsletterStatsOfEmail(String.valueOf(emailId))) {
+           String country, county;
+           country = IPHelper.getCountryName(n.getIp());
+           county = IPHelper.getSubISO(n.getIp());
+
+           switch (country) {
+
+               case "Belgium" -> {
+                   try {
+                       json.put("BG", json.getInt("BG") + 1);
+                   } catch (JSONException e) {
+                       json.put("BG",1);
+                   }
+               }
+               case "Netherlands", "Switzerland", "Austria", "Luxembourg" -> {
+                   try {
+                       json.put(IPHelper.getCountryISO(n.getIp()), json.getInt(IPHelper.getCountryISO(n.getIp()) + 1));
+                   } catch (JSONException e) {
+                       json.put(IPHelper.getCountryISO(n.getIp()),1);
+                   }
+               }
+
+               default -> {
+                   try {
+                       json.put(county, json.getInt(county) + 1);
+                   } catch (JSONException e) {
+                       json.put(county,1);
+                   }
+               }
+           }
+       }
+       return json.toString();
+
+    }
+
+    @GetMapping("/getNewsletterGeo")
+    public String getNewsletterGeoTotal() throws JSONException {
+        JSONObject json = new JSONObject();
+
+        for(NewsletterStats n : newsStatsRepo.findAll()) {
+            String country, county;
+            country = IPHelper.getCountryName(n.getIp());
+            county = IPHelper.getSubISO(n.getIp());
+
+            switch (country) {
+
+                case "Belgium" -> {
+                    try {
+                        json.put("BG", json.getInt("BG") + 1);
+                    } catch (JSONException e) {
+                        json.put("BG",1);
+                    }
+                }
+                case "Netherlands", "Switzerland", "Austria", "Luxembourg" -> {
+                    try {
+                        json.put(IPHelper.getCountryISO(n.getIp()), json.getInt(IPHelper.getCountryISO(n.getIp()) + 1));
+                    } catch (JSONException e) {
+                        json.put(IPHelper.getCountryISO(n.getIp()),1);
+                    }
+                }
+
+                default -> {
+                    try {
+                        json.put(county, json.getInt(county) + 1);
+                    } catch (JSONException e) {
+                        json.put(county,1);
+                    }
+                }
+            }
+        }
+        return json.toString();
+    }
+
+    @GetMapping("/getNewsletterList")
+    public String getNewsletterList(int page, int size) throws JSONException {
+        JSONArray array = new JSONArray();
+        for(NewsletterEmails n : newsEmailsRepo.getAllSortedByDate(PageRequest.of(page, size))) {
+            array.put(new JSONObject(getNewsletterCallup(Math.toIntExact(n.getId()))));
+        }
+        return array.toString();
+    }
+
 
 }

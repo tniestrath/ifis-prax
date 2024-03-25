42a4bfd35815bd1cb6210921320775333ed7478e
pondgangster <majorshroom11@gmail.com>
2024-03-25 11:55:36 +0100

    no

--- backend/src/main/java/com/analysetool/repositories/FeatureWishesRepository.java
+++ backend/src/main/java/com/analysetool/repositories/FeatureWishesRepository.java
@@ -2,14 +2,9 @@ package com.analysetool.repositories;
 
 import com.analysetool.modells.FeatureWishes;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface FeatureWishesRepository extends JpaRepository<FeatureWishes, Long> {
 
-    @Query("SELECT f FROM FeatureWishes f WHERE f.email=:email AND f.feature=:feature AND f.isFixed=:isFixed AND f.team=:team AND f.isNew=:isNew")
-    Optional<FeatureWishes> getByAllExceptId(String email, String feature, String team, boolean isNew);
 }

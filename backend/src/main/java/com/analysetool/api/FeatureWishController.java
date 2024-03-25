cd8ee87751a6ee506b36de913b64f1b5005afbfe
pondgangster <majorshroom11@gmail.com>
2024-03-25 10:51:03 +0100

    MAN WAS FÜR PFADE ICH BIN KEIN PFADFINDER

--- backend/src/main/java/com/analysetool/api/FeatureWishController.java
+++ backend/src/main/java/com/analysetool/api/FeatureWishController.java
@@ -53,7 +53,7 @@ public class FeatureWishController {
 
     @GetMapping("/feedbackSite")
     public String getAllAndFeedbackOption() throws IOException {
-        String html = Files.readString(Path.of("/../../../../backend/src/main/resources/feedback.html"));
+        String html = Files.readString(Path.of("../../../../backend/src/main/resources/feedback.html"));
         StringBuilder tableContent = new StringBuilder();
         for(FeatureWishes f : featureRepo.findAll()) {
             tableContent.append("<tr>");
@@ -70,7 +70,7 @@ public class FeatureWishController {
 
     @GetMapping("/testSite")
     public String getTestForSite() throws IOException {
-        return Files.readString(Path.of("/../../../../backend/src/main/resources/feedback.html"));
+        return Files.readString(Path.of("../../../../backend/src/main/resources/feedback.html"));
     }
 
 }

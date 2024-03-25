9a1b4bb087c4092e2d6bd7c0861aff0fdb18d75b
pondgangster <majorshroom11@gmail.com>
2024-03-25 09:56:05 +0100

    Added some FeatureWishes logic, barebone for now

---
+++ backend/src/main/java/com/analysetool/api/FeatureWishController.java
@@ -0,0 +1,73 @@
+package com.analysetool.api;
+
+import com.analysetool.modells.FeatureWishes;
+import com.analysetool.repositories.FeatureWishesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+@CrossOrigin(originPatterns = "*" , allowCredentials = "true")
+@RequestMapping("/feature")
+public class FeatureWishController {
+
+    @Autowired
+    FeatureWishesRepository featureRepo;
+
+    @Modifying
+    @PostMapping("/addWish")
+    public boolean addWish(Boolean isNew, String feature, String team, String email) {
+        if(featureRepo.getByAllExceptId(email, feature, team, isNew).isEmpty()) {
+            FeatureWishes f = new FeatureWishes();
+            f.setEmail(email);
+            f.setFeature(feature);
+            f.setFixed(false);
+            f.setNew(isNew);
+            f.setTeam(team);
+            featureRepo.save(f);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Modifying
+    @PostMapping("/setFixed")
+    public boolean setFixed(long id) {
+        if(featureRepo.findById(id).isPresent()) {
+            FeatureWishes f = featureRepo.findById(id).get();
+            f.setFixed(true);
+            featureRepo.save(f);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @GetMapping("/feedbackSite")
+    public String getAllAndFeedbackOption() throws IOException {
+        String html = Files.readString(Path.of("feedback.html"));
+        StringBuilder tableContent = new StringBuilder();
+        for(FeatureWishes f : featureRepo.findAll()) {
+            tableContent.append("<tr>");
+
+            tableContent.append("<td>").append(f.getTeam()).append("</td>");
+            tableContent.append("<td>").append(f.getFeature()).append("</td>");
+            tableContent.append("<td>").append(f.isFixed()).append("</td>");
+
+            tableContent.append(tableContent.append("</tr>"));
+        }
+
+        return html;
+    }
+
+    @GetMapping("/testSite")
+    public String getTestForSite() throws IOException {
+        return Files.readString(Path.of("feedback.html"));
+    }
+
+}

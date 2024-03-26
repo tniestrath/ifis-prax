b905f407e3ce6e0b2b50939b75cd7822e6b91f53
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2024-03-26 10:26:33 +0100

    post-display.component, video color, seo tooltip

--- frontend/src/app/component/post/post-display/post-display.component.ts
+++ frontend/src/app/component/post/post-display/post-display.component.ts
@@ -5,13 +5,13 @@ import {Post} from "../Post";
 @Component({
   selector: 'dash-post-display',
   templateUrl: './post-display.component.html',
-  styleUrls: ['./post-display.component.css']
+  styleUrls: ['./post-display.component.css', "../../dash-base/dash-base.component.css"]
 })
 export class PostDisplayComponent extends DashBaseComponent implements OnInit{
-  private post = new Post();
+  post = new Post();
 
   ngOnInit(): void {
-    this.db.getPostById(1312).then(res => {
+    this.db.getPostByIdWithContent(35154).then(res => {
       this.post = res;
     })
   }

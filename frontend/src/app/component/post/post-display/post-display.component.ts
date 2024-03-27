4f6166ba832e3a564af5490665c92fd09259dc9d
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2024-03-27 13:02:51 +0100

    post display rework

--- frontend/src/app/component/post/post-display/post-display.component.ts
+++ frontend/src/app/component/post/post-display/post-display.component.ts
@@ -10,6 +10,7 @@ import Util from "../../../util/Util";
   styleUrls: ['./post-display.component.css', "../../dash-base/dash-base.component.css"]
 })
 export class PostDisplayComponent extends DashBaseComponent implements OnInit{
+  postImg: string = "";
 
   ngOnInit(): void {
     this.setToolTip("", 1,false);
@@ -30,24 +31,24 @@ export class PostDisplayComponent extends DashBaseComponent implements OnInit{
     var authorBox = document.getElementById("post-display-author");
     var dateBox = document.getElementById("post-display-date");
     var tagBox = document.getElementById("post-display-tags");
-    var imgBox = document.getElementById("post-display-img");
+    var imgBox = document.getElementById("post-display-img") as HTMLImageElement;
 
-    var content = post.content.substring(0, post.content.indexOf("<h2>"));
+    var content = post.content.substring(0, post.content.indexOf("</em>"));
     if (!content.startsWith("<strong>")) {
-      content = "<strong>" + post.title + "</strong>" + content;
+      content = "<strong style= 'color: "+ Util.getColor('post', post.type) + " ' >" + post.title + "" + content;
     }
     // @ts-ignore
-    contentBox.innerHTML = content.replace("</strong>", "</strong><br>").replace("</em>", "</em><br><br>");
+    contentBox.innerHTML = content.replace("<strong>", "<strong style= 'color: "+ Util.getColor('post', post.type) + " ' >").replace("</strong>", "</strong><br><br>").replace("</em>", "</em><br><br>");
     // @ts-ignore
-    authorBox.innerText = post.authors;
+    authorBox.innerText = post.authors[0].substring(0, post.authors[0].lastIndexOf(",") > 0 ? post.authors[0].lastIndexOf(",") : 25);
     // @ts-ignore
     dateBox.innerText = post.date;
     // @ts-ignore
-    tagBox.innerText = post.tags;
+    tagBox.innerText = Util.formatArray(post.tags);
     // @ts-ignore
     tagBox.style.backgroundColor = Util.getColor("post", post.type);
     // @ts-ignore
-    imgBox.style.background = "url('" + post.img +"')";
+    this.postImg = post.img;
   }
 
 }
--- frontend/src/app/util/Util.ts
+++ frontend/src/app/util/Util.ts
@@ -158,6 +158,10 @@ export default class Util {
     return array[s1.length][s2.length];
   };
 
+  static formatArray(array : string[]) : string{
+    return array.toString().replace("[", "").replace("]", "");
+  }
+
 }
 export enum DashColors {
   GREEN = "rgb(134,218,118)",
@@ -216,25 +220,4 @@ export enum DashColors {
 
 }
 
-// @ts-ignore
-String.prototype.LevenshteinDistance = function (s2) {
-  var array = new Array(this.length + 1);
-  for (var i = 0; i < this.length + 1; i++)
-    array[i] = new Array(s2.length + 1);
 
-  for (var i = 0; i < this.length + 1; i++)
-    array[i][0] = i;
-  for (var j = 0; j < s2.length + 1; j++)
-    array[0][j] = j;
-
-  for (var i = 1; i < this.length + 1; i++) {
-    for (var j = 1; j < s2.length + 1; j++) {
-      if (this[i - 1] == s2[j - 1]) array[i][j] = array[i - 1][j - 1];
-      else {
-        array[i][j] = Math.min(array[i][j - 1] + 1, array[i - 1][j] + 1);
-        array[i][j] = Math.min(array[i][j], array[i - 1][j - 1] + 1);
-      }
-    }
-  }
-  return array[this.length][s2.length];
-};

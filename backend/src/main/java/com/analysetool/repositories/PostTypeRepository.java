3cc6e374a714e53321b7b87cf2ad492ea8c3f3eb
pondgangster <majorshroom11@gmail.com>
2024-04-10 11:40:26 +0200

    ?

--- backend/src/main/java/com/analysetool/repositories/PostTypeRepository.java
+++ backend/src/main/java/com/analysetool/repositories/PostTypeRepository.java
@@ -18,6 +18,7 @@ public interface PostTypeRepository extends JpaRepository<PostTypes, Long> {
     @Query("SELECT p.post_id FROM PostTypes p WHERE p.type =:type")
     List<Integer> getPostsByType(String type);
 
+    @Query("SELECT p.post_id FROM PostTypes p WHERE p.type=:type")
     List<Integer> getPostsByTypePageable(String type, Pageable pageable);
 
     @Query("SELECT p.post_id FROM PostTypes p WHERE p.type =:type")

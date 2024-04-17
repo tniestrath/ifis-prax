ae2ce3b0235e758c90011d1105ca166081f494a2
pondgangster <majorshroom11@gmail.com>
2024-04-17 10:59:47 +0200

    fixed getViewsDaysBack

--- backend/src/main/java/com/analysetool/repositories/TagStatRepository.java
+++ backend/src/main/java/com/analysetool/repositories/TagStatRepository.java
@@ -29,7 +29,7 @@ public interface TagStatRepository extends JpaRepository<TagStat, Long> {
     @Query("SELECT SUM(t.views) FROM TagStat t WHERE t.uniId + 7 >= (SELECT u.id FROM UniversalStats u ORDER BY u.id DESC LIMIT 1)")
     double getRelevance(int tagId);
 
-    @Query("SELECT t FROM TagStat t WHERE t.uniId + :daysBack >= (SELECT u.id FROM UniversalStats u ORDER BY u.id DESC LIMIT 1)")
+    @Query("SELECT SUM(t.views) FROM TagStat t WHERE t.uniId + :daysBack >= (SELECT u.id FROM UniversalStats u ORDER BY u.id DESC LIMIT 1)")
     int getViewsDaysBack(int daysBack);
 
 }

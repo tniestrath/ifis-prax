ca9798a3e9fd9f92d0c6e1720952772ef351a0d3
pondgangster <majorshroom11@gmail.com>
2024-04-22 12:29:35 +0200

    setUp for Tag rework

--- backend/src/main/java/com/analysetool/repositories/TagCatStatRepository.java
+++ backend/src/main/java/com/analysetool/repositories/TagCatStatRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TagCatStatRepository extends JpaRepository<TagCatStat, Long> {
 
     @Query("SELECT t FROM TagCatStat t WHERE t.tagId=:tagId")
-    TagCatStat getTagCatStatByTagId(int tagId);
+    List<TagCatStat> getTagCatStatByTagId(int tagId);
+
+    @Query("SELECT SUM(t.views) FROM TagCatStat t WHERE t.tagId=:tagId")
+    int getSumViewsByTag(int tagId);
 
 }

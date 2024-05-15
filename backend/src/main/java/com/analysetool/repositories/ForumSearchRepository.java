9db4a262bee2e727cbb313088edc117aa61116c9
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2024-05-15 16:47:58 +0200

    forum search modell & repo

---
+++ backend/src/main/java/com/analysetool/repositories/ForumSearchRepository.java
@@ -0,0 +1,7 @@
+package com.analysetool.repositories;
+
+import com.analysetool.modells.ForumSearch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForumSearchRepository extends JpaRepository<ForumSearch, Long> {
+}

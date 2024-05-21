5a6734aa9c6ca50915632e689b6d1c3764fc6435
tniestrath <Tim.Niestrath@studmail.w-hs.de>
2024-05-21 16:40:59 +0200

    getRankedDiscussion

--- backend/src/main/java/com/analysetool/services/ForumService.java
+++ backend/src/main/java/com/analysetool/services/ForumService.java
@@ -3,8 +3,16 @@ package com.analysetool.services;
 import com.analysetool.modells.ForumDiskussionsthemenClicksByHour;
 import com.analysetool.modells.ForumSearch;
 import com.analysetool.modells.ForumTopicsClicksByHour;
+import com.analysetool.modells.WPWPForoForum;
 import com.analysetool.repositories.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -62,4 +70,28 @@ public class ForumService {
         Integer uniId = uniRepo.getLatestUniStat().getId();
         return clicksByHourRepo.getClicksOfDay(id,uniId);
     }
+
+    public String getRankedDiscussion(int page, int size) throws JSONException {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("clicks").descending());
+        Page<ForumDiskussionsthemenClicksByHour> forumClicksPage = clicksByHourRepo.findAll(pageable);
+
+        JSONArray jsonArray = new JSONArray();
+
+        for (ForumDiskussionsthemenClicksByHour forumClicks : forumClicksPage) {
+            WPWPForoForum forum = forumRepo.findById(forumClicks.getForumId()).orElse(null);
+            if (forum != null) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("title", forum.getTitle());
+                jsonObject.put("clicks", forumClicks.getClicks());
+                jsonObject.put("lastPostDate", forum.getLastPostDate().toString());
+
+                jsonArray.put(jsonObject);
+            }
+        }
+
+        return jsonArray.toString();
+    }
+
+
+
 }

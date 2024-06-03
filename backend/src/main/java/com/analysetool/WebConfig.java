102172d36822c6cddefc658bb803f9d37b4efdff
pondgangster <majorshroom11@gmail.com>
2024-06-03 09:41:24 +0200

    added a CookieEater, if user does not provide a cookie, he will not let him through

--- backend/src/main/java/com/analysetool/WebConfig.java
+++ backend/src/main/java/com/analysetool/WebConfig.java
@@ -2,10 +2,17 @@ package com.analysetool;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final CookieEater cookieEater;
+
+    public WebConfig(CookieEater cookieEater) {
+        this.cookieEater = cookieEater;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -13,5 +20,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(cookieEater)
+                .addPathPatterns("/api/**").excludePathPatterns("/api/forum/**");
     }
 }

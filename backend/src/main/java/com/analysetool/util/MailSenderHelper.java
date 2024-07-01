386c7b0ef0cac41e25fdbbec2b6848582af705ef
pondgangster <majorshroom11@gmail.com>
2024-07-01 11:00:15 +0200

    removed props

--- backend/src/main/java/com/analysetool/util/MailSenderHelper.java
+++ backend/src/main/java/com/analysetool/util/MailSenderHelper.java
@@ -44,7 +44,6 @@ public class MailSenderHelper {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.debug", "true");
 
         return mailSender;

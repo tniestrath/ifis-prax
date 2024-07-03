54382324ed61502e25eda2c9fad7c9cca35deb04
pondgangster <majorshroom11@gmail.com>
2024-07-03 11:57:32 +0200

    filled service to avoid circle and to enhance style

--- backend/src/main/java/com/analysetool/api/UserController.java
+++ backend/src/main/java/com/analysetool/api/UserController.java
@@ -2,7 +2,7 @@ package com.analysetool.api;
 
 import com.analysetool.modells.*;
 import com.analysetool.repositories.*;
-import com.analysetool.services.MailGeneratorService;
+import com.analysetool.services.UserService;
 import com.analysetool.services.PostClicksByHourDLCService;
 import com.analysetool.services.SocialsImpressionsService;
 import com.analysetool.services.UserViewsByHourDLCService;
@@ -87,7 +87,7 @@ public class UserController {
     @Autowired
     private NewsletterRepository newsletterRepo;
     @Autowired
-    private MailGeneratorService mailGeneratorService;
+    private UserService userService;
 
     private final DashConfig config;
 
@@ -339,7 +339,7 @@ public class UserController {
     @GetMapping("/generateMailSingle")
     public boolean generateMailSingle(int userId) {
         try {
-            mailGeneratorService.generateMailSingle(userId);
+            userService.generateMailSingle(userId);
             return true;
         } catch(Exception e) {
             return false;
@@ -1413,20 +1413,8 @@ public class UserController {
         return "none";
     }
 
-    private String getTypeProfileTags(int id) {
-        if (wpUserMetaRepository.existsByUserId((long) id)){
-            String wpUserMeta = wpUserMetaRepository.getWPUserMetaValueByUserId((long) id);
-            if (wpUserMeta.contains("customer")) return "none";
-            if (wpUserMeta.contains("administrator")) return "admin";
-            if (wpUserMeta.contains(Constants.getInstance().getPlusAnbieter())) return "plus";
-            if (wpUserMeta.contains(Constants.getInstance().getBasisPlusAnbieter())) return "basis_plus";
-            if (wpUserMeta.contains(Constants.getInstance().getPremiumAnbieter())) return "premium";
-            if(wpUserMeta.contains(Constants.getInstance().getBasisAnbieter())) return "basis";
-            if (wpUserMeta.contains("anbieter")) return "none";
-        }
-
-
-        return "none";
+    public String getTypeProfileTags(int id) {
+        return userService.getTypeProfileTags(id);
     }
 
     @GetMapping("/hasPost")
@@ -1764,13 +1752,7 @@ public class UserController {
      * @return a List of Strings, containing all Tags in this String.
      */
     public List<String> decryptTags(String cryptedTag) {
-        Pattern cleaner = Pattern.compile("\"([^\"]+)\"");
-        Matcher matcher = cleaner.matcher(cryptedTag);
-        List<String> tags = new ArrayList<>();
-        while(matcher.find()) {
-            tags.add(matcher.group(1));
-        }
-        return tags;
+        return userService.decryptTags(cryptedTag);
     }
 
     /**
@@ -1779,11 +1761,7 @@ public class UserController {
      * @return a List of a List of Strings, containing all Tags in this String.
      */
     public List<List<String>> decryptTagsStringInList(List<String> cryptedTags) {
-        List<List<String>> list = new ArrayList<>();
-        for(String tags : cryptedTags) {
-            list.add(decryptTags(tags));
-        }
-        return list;
+        return userService.decryptTagsStringInList(cryptedTags);
     }
 
 
@@ -1848,14 +1826,7 @@ public class UserController {
     }
 
     public double getUserCountAsPercentageForSingleTag(String tag) {
-        int totalUsersWithTag = getTotalCountOfUsersWithTags();
-        int countForTag = countUsersByTag(tag);
-
-        if (totalUsersWithTag == 0) {
-            return 0; // Vermeidung der Division durch null
-        }
-
-        return (double) countForTag / totalUsersWithTag * 100;
+        return userService.getUserCountAsPercentageForSingleTag(tag);
     }
 
     public Map<String, Double> getPercentageForMultipleTags(List<String> tags) {
@@ -1870,20 +1841,7 @@ public class UserController {
     }
 
     public JSONObject getPercentageForTagsByUserId(Long userId) throws JSONException {
-        JSONObject tagPercentages = new JSONObject();
-        Optional<String> tagData = getTags(userId, getTypeProfileTags(Math.toIntExact(userId)));
-
-        if (tagData.isPresent()) {
-            List<String> rawTags = Arrays.asList(tagData.get().split(";"));
-            List<List<String>> decryptedTags = decryptTagsStringInList(rawTags);
-            for(List<String> tags : decryptedTags) {
-                for (String tag : tags) {
-                    double percentage = getUserCountAsPercentageForSingleTag(tag);
-                    tagPercentages.put(tag, percentage);
-                }
-            }
-        }
-        return tagPercentages;
+        return userService.getPercentageForTagsByUserId(userId);
     }
 
 
@@ -1908,23 +1866,7 @@ public class UserController {
      */
     @GetMapping("/getCompetitionByTagsForUser")
     public Map<String,String> getCompetitionByTags(Long userId){
-        Map<String, String> tagsWithCompetingUsers = new HashMap<>();
-        Optional<String> tagData = getTags(userId, getTypeProfileTags(Math.toIntExact(userId)));
-
-        if (tagData.isPresent()) {
-            List<String> rawTags = Arrays.asList(tagData.get().split(";"));
-            List<List<String>> decryptedTags = decryptTagsStringInList(rawTags);
-
-            for(List<String> tags : decryptedTags) {
-                for (String tag : tags) {
-                    List<Long> competingUserIdsWithTag = getUserIdsByTag(tag);
-                    List<String> competingUsersWithTag = userRepository.findAllDisplayNameByIdIn(competingUserIdsWithTag);
-                    tagsWithCompetingUsers.put(tag, competingUsersWithTag.toString());
-                }
-            }
-        }
-
-        return tagsWithCompetingUsers;
+        return userService.getCompetitionByTags(userId);
     }
 
     /**
@@ -1955,107 +1897,13 @@ public class UserController {
 
     @GetMapping("/getSingleUserTagsData")
     public String getSingleUserTagsData(long id, String sorter) throws JSONException {
-        JSONObject ranking = new JSONObject(getRankingsInTagsForUserBySorter(id, sorter));
-        JSONObject percentage = getPercentageForTagsByUserId(id);
-        var jsonKeys = ranking.keys();
-        JSONArray array = new JSONArray();
-
-        while (jsonKeys.hasNext()) {
-            String tag = jsonKeys.next().toString();
-            JSONObject tempJson;
-            int companyCount = new ArrayList<>(Arrays.stream(getCompetitionByTags(id).get(tag).split(",")).toList()).size();
-            try {
-                 tempJson = new JSONObject().put("percentage", percentage.getDouble(tag));
-            } catch (Exception e) {
-                e.printStackTrace();
-                tempJson = new JSONObject().put("percentage", -1);
-            }
-            try {
-                tempJson.put("ranking", ranking.getInt(tag)).put("name", tag);
-            } catch (Exception e) {
-                e.printStackTrace();
-                tempJson.put("ranking", -1);
-            }
-            tempJson.put("count", companyCount);
-
-            array.put(tempJson);
-        }
-        return array.toString();
-
+        return userService.getSingleUserTagsData(id, sorter);
     }
 
     @GetMapping("/getRankingInTag")
     public String getRankingsInTagsForUserBySorter(long id, String sorter) throws JSONException {
-        Map<String, String> competition = getCompetitionByTags(id);
-        String thisCompanyName = userRepository.getDisplayNameById(id);
-
-        JSONObject json = new JSONObject();
-
-        for(String key : competition.keySet()) {
-            List<String> companyNames = new ArrayList<>(Arrays.stream(competition.get(key).split(",")).toList());
-
-            for(String name : companyNames) {
-                if(name.startsWith(" ")) {
-                    companyNames.set(companyNames.indexOf(name), name.replaceFirst(" ", ""));
-                }
-            }
-
-            if(sorter.equalsIgnoreCase("content")) {
-                json.put(key, getRankingInListByContentView(thisCompanyName, companyNames));
-            } else if(sorter.equalsIgnoreCase("profile")){
-                json.put(key, getRankingInListByProfileView(thisCompanyName, companyNames));
-            } else {
-                json.put(key, getRankingInListByProfileView(thisCompanyName, companyNames));
-            }
-
-        }
-        return json.toString();
-
+        return userService.getRankingsInTagsForUserBySorter(id, sorter);
     }
-
-    private int getRankingInListByProfileView(String companyName, List<String> otherCompanies) {
-        List<String> allCompaniesList = new ArrayList<>(otherCompanies);
-        if(!allCompaniesList.contains(companyName)) {
-            allCompaniesList.add(companyName);
-        }
-        try {
-            allCompaniesList.sort((o1, o2) -> {
-                    int value1 = 0;
-                    int value2 = 0;
-                    if(userRepository.findByDisplayName(o2).isPresent()) {
-                        value2 = userStatsRepository.findByUserId(userRepository.findByDisplayName(o2).get().getId()) != null ? Math.toIntExact(userStatsRepository.findByUserId(userRepository.findByDisplayName(o2).get().getId()).getProfileView()) : 0;
-                    }
-                    if(userRepository.findByDisplayName(o1).isPresent()) {
-                        value1 = userStatsRepository.findByUserId(userRepository.findByDisplayName(o1).get().getId()) != null ? Math.toIntExact(userStatsRepository.findByUserId(userRepository.findByDisplayName(o1).get().getId()).getProfileView()) : 0;
-                    }
-                    return value2 - value1;
-            });
-            return allCompaniesList.indexOf(companyName) + 1;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
-
-    }
-
-    private int getRankingInListByContentView(String companyName, List<String> otherCompanies) {
-        List<String> allCompaniesList = new ArrayList<>(otherCompanies);
-        if(!allCompaniesList.contains(companyName)) {
-            allCompaniesList.add(companyName);
-        }
-        try {
-        allCompaniesList.sort((o1, o2) -> Math.toIntExact((int)(userRepository.findByDisplayName(o2).isPresent() ?
-                postController.getPostViewsOfUserById(userRepository.findByDisplayName(o2).get().getId()) : 0)
-                - (userRepository.findByDisplayName(o1).isPresent() ?
-                postController.getPostViewsOfUserById(userRepository.findByDisplayName(o1).get().getId()) : 0)));
-
-        return allCompaniesList.indexOf(companyName) + 1;
-        } catch (Exception e) {
-            e.printStackTrace();
-        return -1;
-        }
-    }
-
 
 
     //Aggregate Queries for profile tags
@@ -2070,39 +1918,20 @@ public class UserController {
     }
 
     public List<Long> getUserIdsByTag(String tag) {
-        List<Long> list = new ArrayList<>();
-        list.addAll(wpUserMetaRepository.getUserIdsByTagBasis("\"" + tag + "\""));
-        list.addAll(wpUserMetaRepository.getUserIdsByTagBasisPlus("\"" + tag + "\""));
-        list.addAll(wpUserMetaRepository.getUserIdsByTagPlus("\"" + tag + "\""));
-        list.addAll(wpUserMetaRepository.getUserIdsByTagPremium("\"" + tag + "\""));
-        return list;
+        return userService.getUserIdsByTag(tag);
     }
 
     public Integer getTotalCountOfUsersWithTags() {
-        return wpUserMetaRepository.getTotalCountOfUsersWithTagBasis() + wpUserMetaRepository.getTotalCountOfUsersWithTagBasisPlus() + wpUserMetaRepository.getTotalCountOfUsersWithTagPlus() + wpUserMetaRepository.getTotalCountOfUsersWithTagPremium();
+        return userService.getTotalCountOfUsersWithTags();
     }
 
     @GetMapping("/countUsersByTag")
     public Integer countUsersByTag(String tag) {
-        return wpUserMetaRepository.countUsersByTagBasis("\"" + tag + "\"") + wpUserMetaRepository.countUsersByTagBasisPlus("\"" + tag + "\"") + wpUserMetaRepository.countUsersByTagPlus("\"" + tag + "\"") + wpUserMetaRepository.countUsersByTagPremium("\"" + tag + "\"");
+        return userService.countUsersByTag(tag);
     }
 
     public Optional<String> getTags(long userId, String type) {
-        switch (type) {
-            case "basis" -> {
-                return wpUserMetaRepository.getTagsBasis(userId);
-            }
-            case "basis_plus" -> {
-                return wpUserMetaRepository.getTagsBasisPlus(userId);
-            }
-            case "plus" -> {
-                return wpUserMetaRepository.getTagsPlus(userId);
-            }
-            case "premium" -> {
-                return wpUserMetaRepository.getTagsPremium(userId);
-            }
-        }
-        return Optional.empty();
+        return userService.getTags(userId, type);
     }
 
     public List<Long> getAllUserIdsWithTags() {
---
+++ backend/src/main/java/com/analysetool/services/UserService.java
@@ -0,0 +1,318 @@
+package com.analysetool.services;
+
+import com.analysetool.api.PostController;
+import com.analysetool.modells.StatMails;
+import com.analysetool.modells.WPUser;
+import com.analysetool.repositories.StatMailsRepository;
+import com.analysetool.repositories.UserStatsRepository;
+import com.analysetool.repositories.WPUserMetaRepository;
+import com.analysetool.repositories.WPUserRepository;
+import com.analysetool.util.Constants;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private StatMailsRepository statMailsRepo;
+    @Autowired
+    private WPUserRepository userRepo;
+    @Autowired
+    private PostController postController;
+    @Autowired
+    private UserStatsRepository userStatsRepository;
+    @Autowired
+    private WPUserMetaRepository wpUserMetaRepository;
+
+
+    private final String tableBase = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Analyse ihrer Marktplatz Präsens</title></head><body><table id=\"rankings\"><thead><tr><th>Gewählte Themen</th><th>Ihre Platzierung</th><th>Globale Nutzungshäufigkeit</th></tr></thead><tbody>{{TABLEROW}}</tbody></table></body></html>\n";
+    private final String tablerowBase = "<tr><td id='rankings-1'>REPLACE1</td><td id='rankings-2'>REPLACE2</td><td id='rankings-3'>REPLACE3</td></tr>";
+
+    @Scheduled(cron = "0 0 0 1 */3 ?")
+    public void generateMails() throws JSONException {
+        generateMailsPlus();
+        generateMailsPremium();
+    }
+
+    private void generateMailsPremium() {
+        for(WPUser user : userRepo.getByAboType("premium")) {
+
+        }
+    }
+
+    private void generateMailsPlus() throws JSONException {
+        for(WPUser user : userRepo.getByAboType("plus")) {
+            StatMails statMail;
+            if(statMailsRepo.findByUserId(user.getId()) == null) {
+                statMail = new StatMails();
+                statMail.setUserId(Math.toIntExact(user.getId()));
+            } else {
+                statMail = statMailsRepo.findByUserId(user.getId());
+            }
+
+            statMail.setContent(tableBase.replace("{{TABLEROW}}",  makeMailContent("plus", Math.toIntExact(user.getId()))));
+            statMail.setTimestamp(Timestamp.valueOf(LocalDateTime.now()));
+        }
+    }
+
+    public void generateMailSingle(int userId) throws JSONException {
+        WPUser user = userRepo.findById((long) userId).get();
+        StatMails statMail;
+        if(statMailsRepo.findByUserId(user.getId()) == null) {
+            statMail = new StatMails();
+            statMail.setUserId(Math.toIntExact(user.getId()));
+        } else {
+            statMail = statMailsRepo.findByUserId(user.getId());
+        }
+
+        statMail.setContent(tableBase.replace("{{TABLEROW}}",  makeMailContent("plus", Math.toIntExact(user.getId()))));
+        statMail.setTimestamp(Timestamp.valueOf(LocalDateTime.now()));
+    }
+
+
+    private String makeMailContent(String accType, int userId) throws JSONException {
+        StringBuilder content = new StringBuilder();
+
+        JSONArray array = new JSONArray(getSingleUserTagsData(userId, "profile"));
+
+        for(int i = 0; i < array.length(); i++) {
+            JSONObject obj = array.getJSONObject(i);
+            String row = tablerowBase;
+            row = row.replace("REPLACE1", obj.getString("name")).replace("REPLACE2", obj.getString("ranking")).replace("REPLACE3", obj.getString("count"));
+            content.append(row);
+        }
+
+        return content.toString();
+    }
+
+    public String getSingleUserTagsData(long id, String sorter) throws JSONException {
+        JSONObject ranking = new JSONObject(getRankingsInTagsForUserBySorter(id, sorter));
+        JSONObject percentage = getPercentageForTagsByUserId(id);
+        var jsonKeys = ranking.keys();
+        JSONArray array = new JSONArray();
+
+        while (jsonKeys.hasNext()) {
+            String tag = jsonKeys.next().toString();
+            JSONObject tempJson;
+            int companyCount = new ArrayList<>(Arrays.stream(getCompetitionByTags(id).get(tag).split(",")).toList()).size();
+            try {
+                tempJson = new JSONObject().put("percentage", percentage.getDouble(tag));
+            } catch (Exception e) {
+                e.printStackTrace();
+                tempJson = new JSONObject().put("percentage", -1);
+            }
+            try {
+                tempJson.put("ranking", ranking.getInt(tag)).put("name", tag);
+            } catch (Exception e) {
+                e.printStackTrace();
+                tempJson.put("ranking", -1);
+            }
+            tempJson.put("count", companyCount);
+
+            array.put(tempJson);
+        }
+        return array.toString();
+
+    }
+
+    public String getRankingsInTagsForUserBySorter(long id, String sorter) throws JSONException {
+        Map<String, String> competition = getCompetitionByTags(id);
+        String thisCompanyName = userRepo.getDisplayNameById(id);
+
+        JSONObject json = new JSONObject();
+
+        for(String key : competition.keySet()) {
+            List<String> companyNames = new ArrayList<>(Arrays.stream(competition.get(key).split(",")).toList());
+
+            for(String name : companyNames) {
+                if(name.startsWith(" ")) {
+                    companyNames.set(companyNames.indexOf(name), name.replaceFirst(" ", ""));
+                }
+            }
+
+            if(sorter.equalsIgnoreCase("content")) {
+                json.put(key, getRankingInListByContentView(thisCompanyName, companyNames));
+            } else if(sorter.equalsIgnoreCase("profile")){
+                json.put(key, getRankingInListByProfileView(thisCompanyName, companyNames));
+            } else {
+                json.put(key, getRankingInListByProfileView(thisCompanyName, companyNames));
+            }
+
+        }
+        return json.toString();
+
+    }
+
+    public JSONObject getPercentageForTagsByUserId(Long userId) throws JSONException {
+        JSONObject tagPercentages = new JSONObject();
+        Optional<String> tagData = getTags(userId, getTypeProfileTags(Math.toIntExact(userId)));
+
+        if (tagData.isPresent()) {
+            List<String> rawTags = Arrays.asList(tagData.get().split(";"));
+            List<List<String>> decryptedTags = decryptTagsStringInList(rawTags);
+            for(List<String> tags : decryptedTags) {
+                for (String tag : tags) {
+                    double percentage = getUserCountAsPercentageForSingleTag(tag);
+                    tagPercentages.put(tag, percentage);
+                }
+            }
+        }
+        return tagPercentages;
+    }
+
+    public Map<String,String> getCompetitionByTags(Long userId){
+        Map<String, String> tagsWithCompetingUsers = new HashMap<>();
+        Optional<String> tagData = getTags(userId, getTypeProfileTags(Math.toIntExact(userId)));
+
+        if (tagData.isPresent()) {
+            List<String> rawTags = Arrays.asList(tagData.get().split(";"));
+            List<List<String>> decryptedTags = decryptTagsStringInList(rawTags);
+
+            for(List<String> tags : decryptedTags) {
+                for (String tag : tags) {
+                    List<Long> competingUserIdsWithTag = getUserIdsByTag(tag);
+                    List<String> competingUsersWithTag = userRepo.findAllDisplayNameByIdIn(competingUserIdsWithTag);
+                    tagsWithCompetingUsers.put(tag, competingUsersWithTag.toString());
+                }
+            }
+        }
+
+        return tagsWithCompetingUsers;
+    }
+
+
+    private int getRankingInListByContentView(String companyName, List<String> otherCompanies) {
+        List<String> allCompaniesList = new ArrayList<>(otherCompanies);
+        if(!allCompaniesList.contains(companyName)) {
+            allCompaniesList.add(companyName);
+        }
+        try {
+            allCompaniesList.sort((o1, o2) -> Math.toIntExact((int)(userRepo.findByDisplayName(o2).isPresent() ?
+                    postController.getPostViewsOfUserById(userRepo.findByDisplayName(o2).get().getId()) : 0)
+                    - (userRepo.findByDisplayName(o1).isPresent() ?
+                    postController.getPostViewsOfUserById(userRepo.findByDisplayName(o1).get().getId()) : 0)));
+
+            return allCompaniesList.indexOf(companyName) + 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    private int getRankingInListByProfileView(String companyName, List<String> otherCompanies) {
+        List<String> allCompaniesList = new ArrayList<>(otherCompanies);
+        if(!allCompaniesList.contains(companyName)) {
+            allCompaniesList.add(companyName);
+        }
+        try {
+            allCompaniesList.sort((o1, o2) -> {
+                int value1 = 0;
+                int value2 = 0;
+                if(userRepo.findByDisplayName(o2).isPresent()) {
+                    value2 = userStatsRepository.findByUserId(userRepo.findByDisplayName(o2).get().getId()) != null ? Math.toIntExact(userStatsRepository.findByUserId(userRepo.findByDisplayName(o2).get().getId()).getProfileView()) : 0;
+                }
+                if(userRepo.findByDisplayName(o1).isPresent()) {
+                    value1 = userStatsRepository.findByUserId(userRepo.findByDisplayName(o1).get().getId()) != null ? Math.toIntExact(userStatsRepository.findByUserId(userRepo.findByDisplayName(o1).get().getId()).getProfileView()) : 0;
+                }
+                return value2 - value1;
+            });
+            return allCompaniesList.indexOf(companyName) + 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+    public Optional<String> getTags(long userId, String type) {
+        switch (type) {
+            case "basis" -> {
+                return wpUserMetaRepository.getTagsBasis(userId);
+            }
+            case "basis_plus" -> {
+                return wpUserMetaRepository.getTagsBasisPlus(userId);
+            }
+            case "plus" -> {
+                return wpUserMetaRepository.getTagsPlus(userId);
+            }
+            case "premium" -> {
+                return wpUserMetaRepository.getTagsPremium(userId);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public String getTypeProfileTags(int id) {
+        if (wpUserMetaRepository.existsByUserId((long) id)){
+            String wpUserMeta = wpUserMetaRepository.getWPUserMetaValueByUserId((long) id);
+            if (wpUserMeta.contains("customer")) return "none";
+            if (wpUserMeta.contains("administrator")) return "admin";
+            if (wpUserMeta.contains(Constants.getInstance().getPlusAnbieter())) return "plus";
+            if (wpUserMeta.contains(Constants.getInstance().getBasisPlusAnbieter())) return "basis_plus";
+            if (wpUserMeta.contains(Constants.getInstance().getPremiumAnbieter())) return "premium";
+            if(wpUserMeta.contains(Constants.getInstance().getBasisAnbieter())) return "basis";
+            if (wpUserMeta.contains("anbieter")) return "none";
+        }
+
+
+        return "none";
+    }
+
+    public List<List<String>> decryptTagsStringInList(List<String> cryptedTags) {
+        List<List<String>> list = new ArrayList<>();
+        for(String tags : cryptedTags) {
+            list.add(decryptTags(tags));
+        }
+        return list;
+    }
+
+    public List<String> decryptTags(String cryptedTag) {
+        Pattern cleaner = Pattern.compile("\"([^\"]+)\"");
+        Matcher matcher = cleaner.matcher(cryptedTag);
+        List<String> tags = new ArrayList<>();
+        while(matcher.find()) {
+            tags.add(matcher.group(1));
+        }
+        return tags;
+    }
+
+    public double getUserCountAsPercentageForSingleTag(String tag) {
+        int totalUsersWithTag = getTotalCountOfUsersWithTags();
+        int countForTag = countUsersByTag(tag);
+
+        if (totalUsersWithTag == 0) {
+            return 0; // Vermeidung der Division durch null
+        }
+
+        return (double) countForTag / totalUsersWithTag * 100;
+    }
+
+    public List<Long> getUserIdsByTag(String tag) {
+        List<Long> list = new ArrayList<>();
+        list.addAll(wpUserMetaRepository.getUserIdsByTagBasis("\"" + tag + "\""));
+        list.addAll(wpUserMetaRepository.getUserIdsByTagBasisPlus("\"" + tag + "\""));
+        list.addAll(wpUserMetaRepository.getUserIdsByTagPlus("\"" + tag + "\""));
+        list.addAll(wpUserMetaRepository.getUserIdsByTagPremium("\"" + tag + "\""));
+        return list;
+    }
+
+    public Integer getTotalCountOfUsersWithTags() {
+        return wpUserMetaRepository.getTotalCountOfUsersWithTagBasis() + wpUserMetaRepository.getTotalCountOfUsersWithTagBasisPlus() + wpUserMetaRepository.getTotalCountOfUsersWithTagPlus() + wpUserMetaRepository.getTotalCountOfUsersWithTagPremium();
+    }
+
+    public Integer countUsersByTag(String tag) {
+        return wpUserMetaRepository.countUsersByTagBasis("\"" + tag + "\"") + wpUserMetaRepository.countUsersByTagBasisPlus("\"" + tag + "\"") + wpUserMetaRepository.countUsersByTagPlus("\"" + tag + "\"") + wpUserMetaRepository.countUsersByTagPremium("\"" + tag + "\"");
+    }
+
+}

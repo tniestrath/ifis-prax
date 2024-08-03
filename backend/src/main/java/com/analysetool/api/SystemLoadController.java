43d0f923261684fdd02b39fc617d93e043f2b8b4
Philip <majorshroom11@gmail.com>
2024-08-03 15:07:27 +0200

    refactored SystemLoadController logic to SystemLoadService. (removed unused imports)

--- backend/src/main/java/com/analysetool/api/SystemLoadController.java
+++ backend/src/main/java/com/analysetool/api/SystemLoadController.java
@@ -1,10 +1,7 @@
 package com.analysetool.api;
 
-import com.analysetool.modells.SystemLoad;
 import com.analysetool.services.SystemLoadService;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,63 +24,20 @@ public class SystemLoadController {
 
     @GetMapping("/getHour")
     public int getHour() {
-        return LocalDateTime.now().getHour();
+        return systemLoadService.getHour();
     }
 
     @GetMapping("/average")
-    public String getAverageLoad() {
-        List<SystemLoad> allSystemLoads = systemLoadService.getAllSystemLoads();
-        OptionalDouble averageCpuLoad = allSystemLoads.stream().mapToDouble(SystemLoad::getCpuLoad).average();
-        OptionalDouble averageMemoryLoad = allSystemLoads.stream().mapToDouble(SystemLoad::getMemoryLoad).average();
-
-        return String.format("{\"cpu\": %s, \"memory\": %s}", averageCpuLoad.orElse(0), averageMemoryLoad.orElse(0));
-    }
+    public String getAverageLoad() {return systemLoadService.getAverageLoad();}
 
     @GetMapping("/current")
-    public String getCurrentLoad() throws JSONException {
-        SystemLoad systemLoadNow = systemLoadService.getNow();
-        if (systemLoadNow == null) return "{}";
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("cpu", systemLoadNow.getCpuLoad());
-        jsonObject.put("memory", systemLoadNow.getMemoryLoad());
-        jsonObject.put("networkSent", systemLoadNow.getNetworkSent());
-        jsonObject.put("networkRecv", systemLoadNow.getNetworkRecv());
-
-        return jsonObject.toString();
-    }
+    public String getCurrentLoad() throws JSONException {return systemLoadService.getCurrentLoad();}
 
     @GetMapping("/peak")
-    public String getPeakLoad() {
-        List<SystemLoad> allSystemLoads = systemLoadService.getAllSystemLoads();
-        if (allSystemLoads.isEmpty()) return "{}";
-
-        SystemLoad peakLoad = allSystemLoads.stream()
-                .max(Comparator.comparingDouble(load -> (load.getCpuLoad() + load.getMemoryLoad()) / 2))
-                .orElseThrow();
-
-        return String.format("{\"cpu\": %f, \"memory\": %f, \"timestamp\": %d}",
-                peakLoad.getCpuLoad(), peakLoad.getMemoryLoad(), peakLoad.getTimestamp());
-    }
+    public String getPeakLoad() {return systemLoadService.getPeakLoad();}
 
     @GetMapping("/systemLive")
-    public String getComplete() throws JSONException {
-        List<SystemLoad> allSystemLoads = systemLoadService.getTop60ByTimeDesc();
-        Collections.reverse(allSystemLoads);
-        if (allSystemLoads.isEmpty()) return "{}";
-        JSONObject jsonObject = new JSONObject();
-        List<Double> array_cpu = new ArrayList<>();
-        List<Double> array_memory = new ArrayList<>();
-
-        for (SystemLoad systemLoad : allSystemLoads) {
-            array_cpu.add(systemLoad.getCpuLoad());
-            array_memory.add(systemLoad.getMemoryLoad());
-        }
-        jsonObject.put("cpu_record", new JSONArray(array_cpu));
-        jsonObject.put("memory_record",  new JSONArray(array_memory));
-
-        return jsonObject.toString();
-    }
+    public String getComplete() throws JSONException {return systemLoadService.getComplete();}
 
     /**
      * Berechnet die Festplattennutzung in Prozent.
@@ -95,9 +49,7 @@ public class SystemLoadController {
      * @return Die aktuelle Festplattennutzung in Prozent.
      */
     @GetMapping("/getDiscUsageInPercentage")
-    public String getDiscUsageInPercentage(){
-       return String.valueOf(systemLoadService.getDiskUsagePercentage());
-    }
+    public String getDiscUsageInPercentage(){return systemLoadService.getDiscUsageInPercentage();}
 
     /**
      * Diese Methode liefert Informationen über den Speicherplatz auf der Festplatte, einschließlich
@@ -107,17 +59,6 @@ public class SystemLoadController {
      * @return Eine JSON-String-Repräsentation der Festplattendaten oder "Error" bei Auftreten von Fehlern.
      */
     @GetMapping("/getDiscData")
-    public String getDiscData(){
-        try {
-        JSONObject obj = new JSONObject();
-        obj.put("total disc space",systemLoadService.getTotalDiskSpace());
-        obj.put("used disc Space", systemLoadService.getUsedDiskSpace());
-        obj.put("free disc Space",systemLoadService.getUsableDiskSpace());
-        obj.put("disc usage in percentage",systemLoadService.getDiskUsagePercentage());
-        return obj.toString();}
-        catch(Exception e){
-            return "Error";
-        }
-    }
+    public String getDiscData(){return systemLoadService.getDiscData();}
 }
 
--- backend/src/main/java/com/analysetool/services/SystemLoadService.java
+++ backend/src/main/java/com/analysetool/services/SystemLoadService.java
@@ -3,6 +3,9 @@ package com.analysetool.services;
 import com.analysetool.modells.SystemLoad;
 import com.analysetool.repositories.SystemLoadRepository;
 import jakarta.transaction.Transactional;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,8 @@ import oshi.hardware.NetworkIF;
 import oshi.software.os.FileSystem;
 import oshi.software.os.OSFileStore;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.function.ToLongFunction;
 
 @Service
@@ -140,6 +144,99 @@ public class SystemLoadService {
                 .mapToLong(spaceCalculator)
                 .sum();
     }
+
+
+    public int getHour() {
+        return LocalDateTime.now().getHour();
+    }
+
+
+    public String getAverageLoad() {
+        List<SystemLoad> allSystemLoads = getAllSystemLoads();
+        OptionalDouble averageCpuLoad = allSystemLoads.stream().mapToDouble(SystemLoad::getCpuLoad).average();
+        OptionalDouble averageMemoryLoad = allSystemLoads.stream().mapToDouble(SystemLoad::getMemoryLoad).average();
+
+        return String.format("{\"cpu\": %s, \"memory\": %s}", averageCpuLoad.orElse(0), averageMemoryLoad.orElse(0));
+    }
+
+    public String getCurrentLoad() throws JSONException {
+        SystemLoad systemLoadNow = getNow();
+        if (systemLoadNow == null) return "{}";
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("cpu", systemLoadNow.getCpuLoad());
+        jsonObject.put("memory", systemLoadNow.getMemoryLoad());
+        jsonObject.put("networkSent", systemLoadNow.getNetworkSent());
+        jsonObject.put("networkRecv", systemLoadNow.getNetworkRecv());
+
+        return jsonObject.toString();
+    }
+
+
+    public String getPeakLoad() {
+        List<SystemLoad> allSystemLoads = getAllSystemLoads();
+        if (allSystemLoads.isEmpty()) return "{}";
+
+        SystemLoad peakLoad = allSystemLoads.stream()
+                .max(Comparator.comparingDouble(load -> (load.getCpuLoad() + load.getMemoryLoad()) / 2))
+                .orElseThrow();
+
+        return String.format("{\"cpu\": %f, \"memory\": %f, \"timestamp\": %d}",
+                peakLoad.getCpuLoad(), peakLoad.getMemoryLoad(), peakLoad.getTimestamp());
+    }
+
+
+    public String getComplete() throws JSONException {
+        List<SystemLoad> allSystemLoads = getTop60ByTimeDesc();
+        Collections.reverse(allSystemLoads);
+        if (allSystemLoads.isEmpty()) return "{}";
+        JSONObject jsonObject = new JSONObject();
+        List<Double> array_cpu = new ArrayList<>();
+        List<Double> array_memory = new ArrayList<>();
+
+        for (SystemLoad systemLoad : allSystemLoads) {
+            array_cpu.add(systemLoad.getCpuLoad());
+            array_memory.add(systemLoad.getMemoryLoad());
+        }
+        jsonObject.put("cpu_record", new JSONArray(array_cpu));
+        jsonObject.put("memory_record",  new JSONArray(array_memory));
+
+        return jsonObject.toString();
+    }
+
+    /**
+     * Berechnet die Festplattennutzung in Prozent.
+     * Diese Methode summiert den Gesamtspeicherplatz und den verfügbaren Speicherplatz
+     * aller Laufwerke auf dem System und berechnet den Prozentsatz des genutzten
+     * Speicherplatzes. Dies kann nützlich sein, um einen Überblick über die
+     * Speicherauslastung des Systems zu erhalten.
+     *
+     * @return Die aktuelle Festplattennutzung in Prozent.
+     */
+    public String getDiscUsageInPercentage(){
+        return String.valueOf(getDiskUsagePercentage());
+    }
+
+    /**
+     * Diese Methode liefert Informationen über den Speicherplatz auf der Festplatte, einschließlich
+     * des gesamten Speicherplatzes, des genutzten Speicherplatzes, des freien Speicherplatzes und der
+     * prozentualen Nutzung der Festplatte. Die Daten werden im JSON-Format zurückgegeben.
+     *
+     * @return Eine JSON-String-Repräsentation der Festplattendaten oder "Error" bei Auftreten von Fehlern.
+     */
+    public String getDiscData(){
+        try {
+            JSONObject obj = new JSONObject();
+            obj.put("total disc space", getTotalDiskSpace());
+            obj.put("used disc Space", getUsedDiskSpace());
+            obj.put("free disc Space", getUsableDiskSpace());
+            obj.put("disc usage in percentage", getDiskUsagePercentage());
+            return obj.toString();}
+        catch(Exception e){
+            return "Error";
+        }
+    }
+
 }
 
 

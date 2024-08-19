61bc1701bb7a8af025cb1746a905b6e6bc6e44cf
pondgangster <majorshroom11@gmail.com>
2024-08-19 10:46:26 +0200

    )(

--- backend/src/main/java/com/analysetool/services/PostService.java
+++ backend/src/main/java/com/analysetool/services/PostService.java
@@ -537,10 +537,10 @@ public class PostService {
         if(postRepo.findById(id).isEmpty()) {return "errorPostNotFound";}
 
 
-        if(postRepo.findById(id).isPresent() && (postRepo.findById(id).get().getType().equals("post"))
+        if(postRepo.findById(id).isPresent() && (postRepo.findById(id).get().getType().equals("post")
                 || postRepo.findById(id).get().getType().equals("event")
                 || postRepo.findById(id).get().getType().equals("podcast")
-                || postRepo.findById(id).get().getType().equals("video")) {
+                || postRepo.findById(id).get().getType().equals("video"))) {
 
             if (postTypeRepo.getType((int) id) != null) {
                 if (!postTypeRepo.getType((int) id).contains("cyber-risk")) {

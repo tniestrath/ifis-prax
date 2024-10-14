e19ea52d04f229beee0492a46e12a476aab6afd6
pondgangster <majorshroom11@gmail.com>
2024-10-14 07:46:23 +0200

    typo

--- backend/src/main/java/com/analysetool/repositories/NotificationMailLogRepository.java
+++ backend/src/main/java/com/analysetool/repositories/NotificationMailLogRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface NotificationMailLogRepository extends JpaRepository<NotificationMailLog, Long> {
 
-    @Query("SELECT n FROM NotificationMailLog n WHERE n.sent=0")
+    @Query("SELECT n FROM NotificationMailLog n WHERE n.sent=false")
     List<NotificationMailLog> findAllUnsent();
 
 }

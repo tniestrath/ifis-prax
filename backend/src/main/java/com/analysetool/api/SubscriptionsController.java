b0c051a92f92f887aa56f325573e31425aa7a8c8
pondgangster <majorshroom11@gmail.com>
2024-10-16 09:14:42 +0200

    Subsequent changes made

--- backend/src/main/java/com/analysetool/api/SubscriptionsController.java
+++ backend/src/main/java/com/analysetool/api/SubscriptionsController.java
@@ -15,7 +15,7 @@ public class SubscriptionsController {
     private SubscriptionService subService;
 
     @GetMapping("/custom")
-    public boolean subCustom(String type, String thema, String author, String word, HttpServletRequest request) {return subService.subCustom(type, thema, author, word, request);}
+    public String subCustom(String type, String thema, String author, String word, HttpServletRequest request) {return subService.subCustom(type, thema, author, word, request);}
 
     @GetMapping("/unsubscribe")
     public boolean unsubscribe(String type, String thema, String author, String word, HttpServletRequest request) {return subService.unsubscribe(type, thema, author, word, request);}

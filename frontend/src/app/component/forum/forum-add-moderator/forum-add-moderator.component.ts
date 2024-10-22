e32db2ce060e261354306f7f818dfa84514b9453
JRGreen801 <65957473+JRGreen801@users.noreply.github.com>
2024-10-22 11:26:01 +0200

    NEXT UPDATE CHECK FORM BEFORE SUBMIT

--- frontend/src/app/component/forum/forum-add-moderator/forum-add-moderator.component.ts
+++ frontend/src/app/component/forum/forum-add-moderator/forum-add-moderator.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {DashBaseComponent} from "../../dash-base/dash-base.component";
-import {SysVars} from "../../../services/sys-vars-service";
 
 @Component({
   selector: 'dash-forum-add-moderator',
   templateUrl: './forum-add-moderator.component.html',
-  styleUrls: ['./forum-add-moderator.component.css', "../../dash-base/dash-base.component.css"]
+  styleUrls: ['./forum-add-moderator.component.css', "../../dash-base/dash-base.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ForumAddModeratorComponent extends DashBaseComponent implements OnInit{
 
@@ -23,6 +23,8 @@ export class ForumAddModeratorComponent extends DashBaseComponent implements OnI
   forumName = "";
   selectedModIndex = 0;
   selectedForumIndex = 0;
+  suggestMods = true;
+  suggestForums = true;
 
   ngOnInit(): void {
     this.input_username = document.getElementById("moderator-name-input");
@@ -44,28 +46,27 @@ export class ForumAddModeratorComponent extends DashBaseComponent implements OnI
     });
   }
 
+  onModInput(value? : string) {
+    if (value == null){this.modName = ""; return}
+    this.modName = value;
+  }
+  onForumInput(value? : string) {
+    if (value == null){this.forumName = ""; return}
+    this.forumName = value;
+  }
+
+
   onRightsCheckboxChange($event: Event) {
     (this.input_forum as HTMLInputElement).disabled = ($event.target as HTMLInputElement).checked;
     (this.input_forum as HTMLInputElement).required = !($event.target as HTMLInputElement).checked;
   }
 
   onModKey(value: any){
-    if (value.key == "Enter"){
-      if (this.selectedModIndex == 0){
-        this.modName = (this.input_username as HTMLInputElement).value;
-      }
-      else {
-        this.modName = this.modSuggestions[this.selectedModIndex+1];
-        (this.input_username as HTMLInputElement).value = this.modSuggestions[this.selectedModIndex+1];
-        this.selectedModIndex = 0;
-      }
-      this.modSuggestions = [];
-      this.cdr.detectChanges();
-    }
     if (value.key == "Delete" || value.key == "Backspace"){
       value.preventDefault();
       // @ts-ignore
       this.modName = (this.input_username as HTMLInputElement).value = (this.input_username as HTMLInputElement).value.slice(0, -1);
+      this.suggestMods = true;
       this.cdr.detectChanges();
     }
     if (value.key == "ArrowUp" && this.selectedModIndex > 0){
@@ -78,23 +79,11 @@ export class ForumAddModeratorComponent extends DashBaseComponent implements OnI
   }
 
   onForumKey(value: any){
-    if (value.key == "Enter"){
-      if (this.selectedForumIndex == 0){
-        this.forumName = (this.input_forum as HTMLInputElement).value;
-      }
-      else {
-        this.forumName = this.forumSuggestions[this.selectedForumIndex+1];
-        (this.input_forum as HTMLInputElement).value = this.forumSuggestions[this.selectedForumIndex+1];
-        this.selectedForumIndex = 0;
-      }
-      this.forumSuggestions = [];
-      this.cdr.detectChanges();
-      value.preventDefault();
-    }
     if (value.key == "Delete" || value.key == "Backspace"){
       value.preventDefault();
       // @ts-ignore
       this.forumName = (this.input_forum as HTMLInputElement).value = (this.input_forum as HTMLInputElement).value.slice(0, -1);
+      this.suggestForums = true;
       this.cdr.detectChanges();
     }
     if (value.key == "ArrowUp" && this.selectedForumIndex > 0){
@@ -107,50 +96,58 @@ export class ForumAddModeratorComponent extends DashBaseComponent implements OnI
   }
 
   getModeratorSuggestions(value: string) {
-    if (value == this.lastModInput){
+    if (this.suggestMods){
+      if (value == this.lastModInput){
+        let copy = structuredClone(this.modSuggestions);
+        copy.shift();
+        return copy;
+      }
+      if (value == ""){this.modSuggestions = []; return [];}
+      this.api.getModeratorSuggestions(value).then(res => {
+        res.unshift("");
+        this.modSuggestions =  res;
+        this.cdr.detectChanges();
+      });
+      this.lastModInput = value;
       let copy = structuredClone(this.modSuggestions);
       copy.shift();
       return copy;
     }
-    if (value == ""){this.modSuggestions = []; return [];}
-    this.api.getModeratorSuggestions(value).then(res => {
-      res.unshift("");
-      this.modSuggestions =  res;
-      this.cdr.detectChanges();
-    });
-    this.lastModInput = value;
-    let copy = structuredClone(this.modSuggestions);
-    copy.shift();
-    return copy;
+    return [];
   }
 
   getForumSuggestions(value: string) {
-    if (value == this.lastModInput){
+    if (this.suggestForums){
+      if (value == this.lastForumInput){
+        let copy = structuredClone(this.forumSuggestions);
+        copy.shift();
+        return copy;
+      }
+      if (value == ""){this.forumSuggestions = []; return [];}
+      this.api.getForumSuggestions(value).then(res => {
+        res.unshift("");
+        this.forumSuggestions =  res;
+        this.cdr.detectChanges();
+      });
+      this.lastForumInput = value;
       let copy = structuredClone(this.forumSuggestions);
       copy.shift();
       return copy;
     }
-    if (value == ""){this.forumSuggestions = []; return [];}
-    this.api.getForumSuggestions(value).then(res => {
-      res.unshift("");
-      this.forumSuggestions =  res;
-      this.cdr.detectChanges();
-    });
-    this.lastModInput = value;
-    let copy = structuredClone(this.forumSuggestions);
-    copy.shift();
-    return copy;
+    return [];
   }
 
   onModeratorSuggestionClick(mod : string){
+    this.suggestMods = !this.suggestMods
     this.modName = mod;
-    (this.input_username as HTMLInputElement).value = this.modSuggestions[this.selectedModIndex+1];
+    (this.input_username as HTMLInputElement).value = mod
     this.modSuggestions = [];
   }
 
   onForumSuggestionClick(forum : string){
+    this.suggestForums = !this.suggestForums;
     this.forumName = forum;
-    (this.input_forum as HTMLInputElement).value = this.forumSuggestions[this.selectedForumIndex+1];
+    (this.input_forum as HTMLInputElement).value = forum
     this.forumSuggestions = [];
   }
 
